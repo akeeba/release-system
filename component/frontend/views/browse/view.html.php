4041c98830ed6b42c2c9080da51f10fb0709444d
nicholas <nicholas@5fbba189-5f39-4019-a16b-6abf1605764a>
2011-01-13 15:36:11 +0000

    Refactored front-end HTML views to use common base class
    git-svn-id: https://subversion.assembla.com/svn/akeeba-release-system/trunk@69 5fbba189-5f39-4019-a16b-6abf1605764a

--- component/frontend/views/browse/view.html.php
+++ component/frontend/views/browse/view.html.php
@@ -8,43 +8,10 @@
 
 defined('_JEXEC') or die('Restricted Access');
 
-jimport('joomla.application.component.view');
+require_once( dirname(__FILE__).DS.'..'.DS.'view.html.php' );
 
-class ArsViewBrowse extends JView
+class ArsViewBrowse extends ArsViewBase
 {
-	function  display($tpl = null) {
-		$model = $this->getModel();
-		$task = $model->getState('task','cmd');
-
-		// Call the relevant method
-		$method_name = 'on'.ucfirst($task);
-		if(method_exists($this, $method_name)) {
-			$this->$method_name();
-		} else {
-			$this->onDisplay();
-		}
-
-		// Add the CSS/JS definitions
-		$doc = JFactory::getDocument();
-		if($doc->getType() == 'html') {
-			require_once JPATH_COMPONENT.DS.'helpers'.DS.'includes.php';
-			ArsHelperIncludes::includeMedia();
-		}
-
-		// Pass the data
-		$this->assignRef( 'items',		$model->itemList );
-		$this->assignRef( 'item',		$model->item );
-		$this->assignRef( 'lists',		$model->lists );
-		$this->assignRef( 'pagination',	$model->pagination );
-
-		// Pass the parameters
-		$app = JFactory::getApplication();
-		$params =& $app->getPageParameters('com_ars');
-		$this->assignRef( 'params',		$params );
-
-		parent::display($tpl);
-	}
-
 	function onDisplay()
 	{
 		require_once JPATH_COMPONENT.DS.'helpers'.DS.'html.php';
--- component/frontend/views/category/view.html.php
+++ component/frontend/views/category/view.html.php
@@ -8,43 +8,10 @@
 
 defined('_JEXEC') or die('Restricted Access');
 
-jimport('joomla.application.component.view');
+require_once( dirname(__FILE__).DS.'..'.DS.'view.html.php' );
 
-class ArsViewCategory extends JView
+class ArsViewCategory extends ArsViewBase
 {
-	function  display($tpl = null) {
-		$model = $this->getModel();
-		$task = $model->getState('task','cmd');
-
-		// Call the relevant method
-		$method_name = 'on'.ucfirst($task);
-		if(method_exists($this, $method_name)) {
-			$this->$method_name();
-		} else {
-			$this->onDisplay();
-		}
-
-		// Add the CSS/JS definitions
-		$doc = JFactory::getDocument();
-		if($doc->getType() == 'html') {
-			require_once JPATH_COMPONENT.DS.'helpers'.DS.'includes.php';
-			ArsHelperIncludes::includeMedia();
-		}
-
-		// Pass the data
-		$this->assignRef( 'items',		$model->itemList );
-		$this->assignRef( 'item',		$model->item );
-		$this->assignRef( 'lists',		$model->lists );
-		$this->assignRef( 'pagination',	$model->pagination );
-
-		// Pass the parameters
-		$app = JFactory::getApplication();
-		$params =& $app->getPageParameters('com_ars');
-		$this->assignRef( 'params',		$params );
-
-		parent::display($tpl);
-	}
-
 	function onDisplay()
 	{
 		// Add a breadcrumb if necessary
