99ea412907baae2a457bad5639e0eb06db3b69c6
nikosdion <nikosdion@5fbba189-5f39-4019-a16b-6abf1605764a>
2011-07-11 17:46:40 +0000

    # SQL file didn't run on upgade on Joomla! 1.6/1.7; applying workaround
    git-svn-id: https://subversion.assembla.com/svn/akeeba-release-system/trunk@186 5fbba189-5f39-4019-a16b-6abf1605764a

--- component/script.ars.php
+++ component/script.ars.php
@@ -7,6 +7,34 @@
  */
 
 class Com_ArsInstallerScript {
+	
+	function update($parent) {
+		$db = JFactory::getDBO();
+		if(method_exists($parent, 'extension_root')) {
+			$sqlfile = $parent->getPath('extension_root').DS.'install.sql';
+		} else {
+			$sqlfile = $parent->getParent()->getPath('extension_root').DS.'install.sql';
+		}
+		$buffer = file_get_contents($sqlfile);
+		if ($buffer !== false) {
+			jimport('joomla.installer.helper');
+			$queries = JInstallerHelper::splitSql($buffer);
+			if (count($queries) != 0) {
+				foreach ($queries as $query)
+				{
+					$query = trim($query);
+					if ($query != '' && $query{0} != '#') {
+						$db->setQuery($query);
+						if (!$db->query()) {
+							JError::raiseWarning(1, JText::sprintf('JLIB_INSTALLER_ERROR_SQL_ERROR', $db->stderr(true)));
+							return false;
+						}
+					}
+				}
+			}
+		}
+	}
+	
 	function postflight($type, $parent) {
 		define('_AKEEBA_HACK', 1);
 		require_once('install.ars.php');
