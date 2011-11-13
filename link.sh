5cb92255322e7c71d08c529965987ea1db6ad1bd
nikosdion <nikosdion@5fbba189-5f39-4019-a16b-6abf1605764a>
2011-11-13 12:40:29 +0000

    Adding link.sh project management file and ignoring linked Live Update resources
    git-svn-id: https://subversion.assembla.com/svn/akeeba-release-system/trunk@333 5fbba189-5f39-4019-a16b-6abf1605764a

---
+++ link.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#
+# Link with Live Update
+#
+rm component/backend/liveupdate/LICENSE.txt
+ln -s `pwd`/../liveupdate/code/LICENSE.TXT component/backend/liveupdate/LICENSE.txt
+rm component/backend/liveupdate/assets
+ln -s `pwd`/../liveupdate/code/assets component/backend/liveupdate/assets
+rm component/backend/liveupdate/classes
+ln -s `pwd`/../liveupdate/code/classes component/backend/liveupdate/classes
+rm component/backend/liveupdate/language
+ln -s `pwd`/../liveupdate/code/language component/backend/liveupdate/language
+rm component/backend/liveupdate/liveupdate.php
+ln `pwd`/../liveupdate/code/liveupdate.php component/backend/liveupdate/liveupdate.php
+
+#
+# Link with the OTP plugin
+#
+rm plugins/plg_oneclickaction
+ln -s `pwd`/../liveupdate/plugins/system/oneclickaction plugins/plg_oneclickaction
