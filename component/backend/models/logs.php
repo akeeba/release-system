e4eb6fcd2dfa0f803f64f73b014bd149373351cb
nikosdion <nikosdion@5fbba189-5f39-4019-a16b-6abf1605764a>
2012-06-01 09:32:13 +0000

    Refactored Logs view
    git-svn-id: https://subversion.assembla.com/svn/akeeba-release-system/trunk@457 5fbba189-5f39-4019-a16b-6abf1605764a

---
+++ component/backend/models/logs.php
@@ -0,0 +1,276 @@
+<?php
+/**
+ * @package AkeebaReleaseSystem
+ * @copyright Copyright (c)2010-2012 Nicholas K. Dionysopoulos
+ * @license GNU General Public License version 3, or later
+ */
+
+defined('_JEXEC') or die();
+
+class ArsModelLogs extends FOFModel
+{
+	public function buildQuery($overrideLimits = false)
+	{
+		$db = $this->getDbo();
+		
+		$query = FOFQueryAbstract::getNew($db)
+			->select(array(
+				$db->qn('l').'.*',
+				$db->qn('c').'.'.$db->qn('title').' AS '.$db->qn('category'),
+				$db->qn('r').'.'.$db->qn('version'),
+				$db->qn('r').'.'.$db->qn('maturity'),
+				$db->qn('i').'.'.$db->qn('title').' AS '.$db->qn('item'),
+				'IF('.$db->qn('i').'.'.$db->qn('type').' = '.$db->q('file').','.$db->qn('i').'.'.$db->qn('filename').','.$db->qn('i').'.'.$db->qn('url').') AS '.$db->qn('asset'),
+				$db->qn('i').'.'.$db->qn('updatestream'),
+				$db->qn('i').'.'.$db->qn('filesize'),
+				$db->qn('i').'.'.$db->qn('release_id'),
+				$db->qn('r').'.'.$db->qn('category_id'),
+				$db->qn('u').'.'.$db->qn('name'),
+				$db->qn('u').'.'.$db->qn('username'),
+				$db->qn('u').'.'.$db->qn('email')
+			))
+			->from($db->qn('#__ars_log').' AS '.$db->qn('l'))
+			->join('INNER', $db->qn('#__ars_items').' AS '.$db->qn('i').' ON('.$db->qn('i').'.'.$db->qn('id').' = '.$db->qn('l').'.'.$db->qn('item_id').')')
+			->join('INNER', $db->qn('#__ars_releases').' AS '.$db->qn('r').' ON('.$db->qn('r').'.'.$db->qn('id').' = '.$db->qn('i').'.'.$db->qn('release_id').')')
+			->join('INNER', $db->qn('#__ars_categories').' AS '.$db->qn('c').' ON('.$db->qn('c').'.'.$db->qn('id').' = '.$db->qn('r').'.'.$db->qn('category_id').')')
+			->join('LEFT', $db->qn('#__users').' AS '.$db->qn('u').' ON('.$db->qn('u').'.'.$db->qn('id').' = '.$db->qn('user_id').')')
+			;
+		
+		$fltItemText	= $this->getState('itemtext', null, 'string');
+		$fltUserText	= $this->getState('usertext', null, 'string');
+		$fltReferer		= $this->getState('referer', null, 'string');
+		$fltIP			= $this->getState('ip', null, 'string');
+		$fltCountry		= $this->getState('country', null, 'string');
+		$fltAuthorized	= $this->getState('authorized', null, 'cmd');
+		$fltCategory	= $this->getState('category', null, 'int');
+		$fltVersion		= $this->getState('version', null, 'int');
+		
+		if(!is_null($fltAuthorized) && ($fltAuthorized != '')) {
+			$fltAuthorized = (int)$fltAuthorized;
+		} else {
+			$fltAuthorized = null;
+		}
+		
+		if($fltItemText) {
+			// This extra query approach is required for performance on very large log tables (multiple millions of rows)
+			$itemIDs = $this->getItems($fltItemText);
+			if(empty($itemIDs)) {
+				$query->where('FALSE');
+			} else {
+				$ids = implode(',', $itemIDs);
+				$query->where($db->qn('item_id').' IN('.$ids.')');
+			}
+		}
+		if($fltUserText) {
+			// This extra query approach is required for performance on very large log tables (multiple millions of rows)
+			$userIDs = $this->getUsers($fltUserText);
+			if(empty($userIDs)) {
+				$query->where('FALSE');
+			} else {
+				$ids = implode(',', $userIDs);
+				$query->where($db->qn('user_id').' IN('.$ids.')');
+			}
+		}
+		if($fltReferer) {
+			$query->where($db->qn('referer').' LIKE '.$db->q("%$fltReferer%"));
+		}
+		if($fltIP) {
+			$query->where($db->qn('ip').' LIKE '.$db->q("%$fltIP%"));
+		}
+		if($fltCountry) {
+			$query->where($db->qn('country').' = '.$db->q($fltCountry));
+		}
+		if(is_numeric($fltAuthorized)) {
+			$query->where($db->qn('authorized').' = '.$db->q($fltAuthorized));
+		}
+		if($fltCategory) {
+			$query_inner = FOFQueryAbstract::getNew($db)
+				->select($db->qn('id'))
+				->from($db->qn('#__ars_releases'))
+				->where($db->qn('category_id').' = '.$db->q($fltCategory));
+			$query_outer = FOFQueryAbstract::getNew($db)
+				->select($db->qn('id'))
+				->from($db->qn('#__ars_items'))
+				->where($db->qn('release_id').' IN ('.$query_inner.')');
+			$db->setQuery($query_outer);
+			if(version_compare(JVERSION, '3.0', 'ge')) {
+				$ids = $db->loadColumn();
+			} else {
+				$ids = $db->loadResultArray();
+			}
+			$clause = '('.implode(", ", $ids).')';
+			
+			$query->where($db->qn('item_id').' IN '.$clause);
+		}
+		
+		if($fltVersion) {
+			$query_outer = FOFQueryAbstract::getNew($db)
+				->select($db->qn('id'))
+				->from($db->qn('#__ars_items'))
+				->where($db->qn('release_id').' = '.$db->q($fltVersion));
+			$db->setQuery($query_outer);
+			if(version_compare(JVERSION, '3.0', 'ge')) {
+				$ids = $db->loadColumn();
+			} else {
+				$ids = $db->loadResultArray();
+			}
+			$clause = '('.implode(", ", $ids).')';
+			
+			$query->where($db->qn('item_id').' IN '.$clause);
+		}
+		
+		$order = $this->getState('filter_order', 'id', 'cmd');
+		if(!in_array($order, array_keys($this->getTable()->getData()))) $order = 'id';
+		$dir = $this->getState('filter_order_Dir', 'DESC', 'cmd');
+		$query->order($order.' '.$dir);
+
+		return $query;
+	}
+	
+	public function buildCountQuery() {
+		$db = $this->getDbo();
+		
+		$query = FOFQueryAbstract::getNew($db)
+			->select('COUNT(*)')
+			->from($db->qn('#__ars_log').' AS '.$db->qn('l'))
+			;
+		
+		$fltItemText	= $this->getState('itemtext', null, 'string');
+		$fltUserText	= $this->getState('usertext', null, 'string');
+		$fltReferer		= $this->getState('referer', null, 'string');
+		$fltIP			= $this->getState('ip', null, 'string');
+		$fltCountry		= $this->getState('country', null, 'string');
+		$fltAuthorized	= $this->getState('authorized', null, 'cmd');
+		$fltCategory	= $this->getState('category', null, 'int');
+		$fltVersion		= $this->getState('version', null, 'int');
+		
+		if(!is_null($fltAuthorized) && ($fltAuthorized != '')) {
+			$fltAuthorized = (int)$fltAuthorized;
+		} else {
+			$fltAuthorized = null;
+		}
+		
+		if($fltItemText) {
+			// This extra query approach is required for performance on very large log tables (multiple millions of rows)
+			$itemIDs = $this->getItems($fltItemText);
+			if(empty($itemIDs)) {
+				$query->where('FALSE');
+			} else {
+				$ids = implode(',', $itemIDs);
+				$query->where($db->qn('item_id').' IN('.$ids.')');
+			}
+		}
+		if($fltUserText) {
+			// This extra query approach is required for performance on very large log tables (multiple millions of rows)
+			$userIDs = $this->getUsers($fltUserText);
+			if(empty($userIDs)) {
+				$query->where('FALSE');
+			} else {
+				$ids = implode(',', $userIDs);
+				$query->where($db->qn('user_id').' IN('.$ids.')');
+			}
+		}
+		if($fltReferer) {
+			$query->where($db->qn('referer').' LIKE '.$db->q("%$fltReferer%"));
+		}
+		if($fltIP) {
+			$query->where($db->qn('ip').' LIKE '.$db->q("%$fltIP%"));
+		}
+		if($fltCountry) {
+			$query->where($db->qn('country').' = '.$db->q($fltCountry));
+		}
+		if(is_numeric($fltAuthorized)) {
+			$query->where($db->qn('authorized').' = '.$db->q($fltAuthorized));
+		}
+		if($fltCategory) {
+			$query_inner = FOFQueryAbstract::getNew($db)
+				->select($db->qn('id'))
+				->from($db->qn('#__ars_releases'))
+				->where($db->qn('category_id').' = '.$db->q($fltCategory));
+			$query_outer = FOFQueryAbstract::getNew($db)
+				->select($db->qn('id'))
+				->from($db->qn('#__ars_items'))
+				->where($db->qn('release_id').' IN ('.$query_inner.')');
+			$db->setQuery($query_outer);
+			if(version_compare(JVERSION, '3.0', 'ge')) {
+				$ids = $db->loadColumn();
+			} else {
+				$ids = $db->loadResultArray();
+			}
+			$clause = '('.implode(", ", $ids).')';
+			
+			$query->where($db->qn('item_id').' IN '.$clause);
+		}
+		
+		if($fltVersion) {
+			$query_outer = FOFQueryAbstract::getNew($db)
+				->select($db->qn('id'))
+				->from($db->qn('#__ars_items'))
+				->where($db->qn('release_id').' = '.$db->q($fltVersion));
+			$db->setQuery($query_outer);
+			if(version_compare(JVERSION, '3.0', 'ge')) {
+				$ids = $db->loadColumn();
+			} else {
+				$ids = $db->loadResultArray();
+			}
+			$clause = '('.implode(", ", $ids).')';
+			
+			$query->where($db->qn('item_id').' IN '.$clause);
+		}
+		
+		$order = $this->getState('filter_order', 'id', 'cmd');
+		if(!in_array($order, array_keys($this->getTable()->getData()))) $order = 'id';
+		$dir = $this->getState('filter_order_Dir', 'DESC', 'cmd');
+		$query->order($order.' '.$dir);
+
+		return $query;
+	}
+	
+	/**
+	 * Returns the user IDs whose username, email address or real name contains the $frag string
+	 * @param string $frag
+	 * @return array|null
+	 */
+	private function getUsers($frag)
+	{
+		$db = $this->getDBO();
+		
+		$qfrag = $db->q("%".$frag."%");
+		$query = FOFQueryAbstract::getNew($db)
+			->select($db->qn('id'))
+			->from($db->qn('#__users'))
+			->where($db->qn('name').' LIKE '.$qfrag)
+			->where($db->qn('username').' LIKE '.$qfrag, 'OR')
+			->where($db->qn('email').' LIKE '.$qfrag, 'OR')
+			->where($db->qn('params').' LIKE '.$qfrag, 'OR');
+		
+		$db->setQuery($query);
+		if(version_compare(JVERSION, '3.0', 'ge')) {
+			return $db->loadColumn();
+		} else {
+			return $db->loadResultArray();
+		}
+	}
+	
+	/**
+	 * Gets a list of download item IDs whose title contains the $frag string
+	 * @param string $frag
+	 * @return array|null
+	 */
+	private function getItems($frag)
+	{
+		$db = $this->getDBO();
+		$qfrag = $db->q("%".$frag."%");
+		$query = FOFQueryAbstract::getNew($db)
+			->select($db->qn('id'))
+			->from($db->qn('#__ars_items'))
+			->where($db->qn('title').' LIKE '.$qfrag);
+		
+		$db->setQuery($query);
+		if(version_compare(JVERSION, '3.0', 'ge')) {
+			return $db->loadColumn();
+		} else {
+			return $db->loadResultArray();
+		}
+	}	
+}
---
+++ component/backend/views/logs/tmpl/default.php
@@ -0,0 +1,176 @@
+<?php
+/**
+ * @package AkeebaReleaseSystem
+ * @copyright Copyright (c)2010-2012 Nicholas K. Dionysopoulos
+ * @license GNU General Public License version 3, or later
+ */
+
+defined('_JEXEC') or die();
+
+jimport('joomla.utilities.date');
+
+$this->loadHelper('select');
+
+FOFTemplateUtils::addCSS('media://com_ars/css/backend.css');
+?>
+<form name="adminForm" id="adminForm" action="index.php" method="post">
+	<input type="hidden" name="option" id="option" value="com_ars" />
+	<input type="hidden" name="view" id="view" value="logs" />
+	<input type="hidden" name="task" id="task" value="browse" />
+	<input type="hidden" name="boxchecked" id="boxchecked" value="0" />
+	<input type="hidden" name="hidemainmenu" id="hidemainmenu" value="0" />
+	<input type="hidden" name="filter_order" id="filter_order" value="<?php echo $this->lists->order ?>" />
+	<input type="hidden" name="filter_order_Dir" id="filter_order_Dir" value="<?php echo $this->lists->order_Dir ?>" />
+	<input type="hidden" name="<?php echo JUtility::getToken();?>" value="1" />
+<table class="adminlist">
+	<thead>
+		<tr>
+			<th width="20">
+				<input type="checkbox" name="toggle" value="" onclick="checkAll(<?php echo count( $this->items ) + 1; ?>);" />
+			</th>
+			<th>
+				<?php echo JHTML::_('grid.sort', 'LBL_LOGS_ITEM', 'item', $this->lists->order_Dir, $this->lists->order); ?>
+			</th>
+			<th>
+				<?php echo JHTML::_('grid.sort', 'LBL_LOGS_USER', 'name', $this->lists->order_Dir, $this->lists->order); ?>
+			</th>
+			<th>
+				<?php echo JHTML::_('grid.sort', 'LBL_LOGS_ACCESSED', 'accessed_on', $this->lists->order_Dir, $this->lists->order); ?>
+			</th>
+			<th>
+				<?php echo JHTML::_('grid.sort', 'LBL_LOGS_REFERER', 'referer', $this->lists->order_Dir, $this->lists->order); ?>
+			</th>
+			<th>
+				<?php echo JHTML::_('grid.sort', 'LBL_LOGS_IP', 'ip', $this->lists->order_Dir, $this->lists->order); ?>
+			</th>
+			<th>
+				<?php echo JHTML::_('grid.sort', 'LBL_LOGS_COUNTRY', 'country', $this->lists->order_Dir, $this->lists->order); ?>
+			</th>
+			<th>
+				<?php echo JHTML::_('grid.sort', 'LBL_LOGS_AUTHORIZED', 'authorized', $this->lists->order_Dir, $this->lists->order); ?>
+			</th>
+		</tr>
+		<tr>
+			<td></td>
+			<td>
+				<?php echo ArsHelperSelect::categories($this->getModel()->getState('category'), 'category', array('onchange'=>'this.form.submit();')) ?>
+				<br/>
+				<?php echo ArsHelperSelect::releases($this->getModel()->getState('version'), 'version', array('onchange'=>'this.form.submit();'), $this->getModel()->getState('category')) ?>
+				<br/>
+				<input type="text" name="itemtext" id="itemtext"
+					value="<?php echo $this->escape($this->getModel()->getState('itemtext'));?>"
+					class="text_area" onchange="document.adminForm.submit();" />
+				<button onclick="this.form.submit();">
+					<?php echo JText::_('Go'); ?>
+				</button>
+				<button onclick="document.adminForm.itemtext.value='';this.form.submit();">
+					<?php echo JText::_('Reset'); ?>
+				</button>
+			</td>
+			<td>
+				<input type="text" name="usertext" id="usertext"
+					value="<?php echo $this->escape($this->getModel()->getState('usertext'));?>"
+					class="text_area" onchange="document.adminForm.submit();" />
+				<button onclick="this.form.submit();">
+					<?php echo JText::_('Go'); ?>
+				</button>
+				<button onclick="document.adminForm.usertext.value='';this.form.submit();">
+					<?php echo JText::_('Reset'); ?>
+				</button>
+			</td>
+			<td>&nbsp;</td>
+			<td>
+				<input type="text" name="referer" id="referer"
+					value="<?php echo $this->escape($this->getModel()->getState('referer'));?>"
+					class="text_area" onchange="document.adminForm.submit();" />
+				<button onclick="this.form.submit();">
+					<?php echo JText::_('Go'); ?>
+				</button>
+				<button onclick="document.adminForm.referer.value='';this.form.submit();">
+					<?php echo JText::_('Reset'); ?>
+				</button>
+			</td>
+			<td>
+				<input type="text" name="ip" id="ip"
+					value="<?php echo $this->escape($this->getModel()->getState('ip'));?>"
+					class="text_area" onchange="document.adminForm.submit();" />
+				<button onclick="this.form.submit();">
+					<?php echo JText::_('Go'); ?>
+				</button>
+				<button onclick="document.adminForm.ip.value='';this.form.submit();">
+					<?php echo JText::_('Reset'); ?>
+				</button>
+			</td>
+			<td>
+				<?php echo ArsHelperSelect::countries($this->getModel()->getState('country'), 'country', array('onchange'=>'this.form.submit();','style'=>'width: 80px')) ?>
+			</td>
+			<td>
+				<?php echo ArsHelperSelect::booleanlist('authorized', array('onchange'=>'this.form.submit();'), $this->getModel()->getState('authorized')) ?>
+			</td>
+		</tr>
+	</thead>
+	<tfoot>
+		<tr>
+			<td colspan="8">
+				<?php if($this->pagination->total > 0) echo $this->pagination->getListFooter() ?>
+			</td>
+		</tr>
+	</tfoot>
+	<tbody>
+	<?php if($count = count($this->items)): ?>
+		<?php
+			$i = 0;
+			$m = 1;
+
+			foreach($this->items as $item):
+			$m = 1 - $m;
+		?>
+		<tr class="row<?php echo $m?>">
+			<td>
+				<?php echo JHTML::_('grid.id', $i, $item->id, false); ?>
+			</td>
+			<td>
+				<?php echo $this->escape($item->category) ?>
+				<?php echo $this->escape($item->version) ?> <br/>
+				<small><?php echo $this->escape($item->item) ?></small>
+			</td>
+			<td>
+				<strong><?php echo $this->escape($item->name) ?></strong><br/>
+				<small>
+					<?php echo $this->escape($item->username) ?> &bull;
+					<?php echo $this->escape($item->email) ?>
+				</small>
+			</td>
+			<td>
+				<?php echo $this->escape($item->accessed_on) ?>
+			</td>
+			<td>
+				<div style="width: 200px; overflow: hidden;">
+					<a href="<?php echo $this->escape($item->referer) ?>" target="_blank">
+						<?php echo $this->escape($item->referer, -15) ?>
+					</a>
+				</div>
+			</td>
+			<td>
+				<tt><?php echo $this->escape($item->ip); ?></tt>
+			</td>
+			<td>
+				<?php echo $this->escape(ArsHelperSelect::decodeCountry($item->country)) ?>
+			</td>
+			<td>
+				<?php echo $item->authorized ? JHTML::_('image','admin/tick.png', '', array('border' => 0), true) : JHTML::_('image','admin/publish_x.png', '', array('border' => 0), true); ?>
+			</td>
+		</tr>
+	<?php
+			$i++;
+			endforeach;
+	?>
+	<?php else : ?>
+		<tr>
+			<td colspan="8" align="center"><?php echo JText::_('LBL_ARS_NOITEMS') ?></td>
+		</tr>
+	<?php endif ?>
+	</tbody>
+</table>
+
+</form>
