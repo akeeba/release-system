208e0c5aa0a0d1b4236ff3bffa3090ce712c5780
nikosdion <nikosdion@5fbba189-5f39-4019-a16b-6abf1605764a>
2012-06-04 17:17:12 +0000

    Backend toolbar cleanup
    git-svn-id: https://subversion.assembla.com/svn/akeeba-release-system/trunk@467 5fbba189-5f39-4019-a16b-6abf1605764a

--- component/backend/toolbar.php
+++ component/backend/toolbar.php
@@ -9,18 +9,83 @@ defined('_JEXEC') or die();
 
 class ArsToolbar extends FOFToolbar
 {
+	protected function getMyViews()
+	{
+		$views = array(
+			'cpanels',
+			'categories',
+			'releases',
+			'items',
+			'impjeds'
+		);
+		return $views;
+	}
+	
+	public function onBrowse()
+	{
+		parent::onBrowse();
+		
+		JToolBarHelper::divider();
+		JToolBarHelper::back('COM_ARS_TITLE_CPANELS', 'index.php?option=com_ars');
+	}
+	
 	public function onImpjeds()
 	{
+		JToolBarHelper::title(JText::_( FOFInput::getCmd('option','com_foobar',$this->input)).' &ndash; <small>'.JText::_('COM_ARS_TITLE_IMPJEDS').'</small>', str_replace('com_', '', FOFInput::getCmd('option','com_foobar',$this->input)));
+		JToolBarHelper::back('COM_ARS_TITLE_CPANELS', 'index.php?option=com_ars');
+		
+		$this->renderSubmenu();
+	}
+	
+	public function onEnvironmentsBrowse()
+	{
+		// Set toolbar title
 		$subtitle_key = FOFInput::getCmd('option','com_foobar',$this->input).'_TITLE_'.strtoupper(FOFInput::getCmd('view','cpanel',$this->input));
 		JToolBarHelper::title(JText::_( FOFInput::getCmd('option','com_foobar',$this->input)).' &ndash; <small>'.JText::_($subtitle_key).'</small>', str_replace('com_', '', FOFInput::getCmd('option','com_foobar',$this->input)));
+
+		// Add toolbar buttons
+		if($this->perms->create) {
+			JToolBarHelper::addNewX();
+		}
+		if($this->perms->edit) {
+			JToolBarHelper::editListX();
+		}
+		if($this->perms->create || $this->perms->edit) {
+			JToolBarHelper::divider();
+		}
+
+		if($this->perms->delete) {
+			$msg = JText::_(FOFInput::getCmd('option','com_foobar',$this->input).'_CONFIRM_DELETE');
+			JToolBarHelper::deleteList($msg);
+		}
 		
+		JToolBarHelper::divider();
+		JToolBarHelper::back('COM_ARS_TITLE_CPANELS', 'index.php?option=com_ars');
+
+		$this->renderSubmenu();
+	}
+	
+	public function onLogsBrowse()
+	{
+		// Set toolbar title
+		$subtitle_key = FOFInput::getCmd('option','com_foobar',$this->input).'_TITLE_'.strtoupper(FOFInput::getCmd('view','cpanel',$this->input));
+		JToolBarHelper::title(JText::_( FOFInput::getCmd('option','com_foobar',$this->input)).' &ndash; <small>'.JText::_($subtitle_key).'</small>', str_replace('com_', '', FOFInput::getCmd('option','com_foobar',$this->input)));
+
+		if($this->perms->delete) {
+			$msg = JText::_(FOFInput::getCmd('option','com_foobar',$this->input).'_CONFIRM_DELETE');
+			JToolBarHelper::deleteList($msg);
+		}
+		
+		JToolBarHelper::divider();
+		JToolBarHelper::back('COM_ARS_TITLE_CPANELS', 'index.php?option=com_ars');
+
 		$this->renderSubmenu();
 	}
 	
 	public function onUploads()
 	{
-		$subtitle_key = FOFInput::getCmd('option','com_foobar',$this->input).'_TITLE_'.strtoupper(FOFInput::getCmd('view','cpanel',$this->input));
-		JToolBarHelper::title(JText::_( FOFInput::getCmd('option','com_foobar',$this->input)).' &ndash; <small>'.JText::_($subtitle_key).'</small>', str_replace('com_', '', FOFInput::getCmd('option','com_foobar',$this->input)));
+		JToolBarHelper::title(JText::_( FOFInput::getCmd('option','com_foobar',$this->input)).' &ndash; <small>'.JText::_('COM_ARS_TITLE_UPLOADS').'</small>', str_replace('com_', '', FOFInput::getCmd('option','com_foobar',$this->input)));
+		JToolBarHelper::back('COM_ARS_TITLE_CPANELS', 'index.php?option=com_ars');
 		
 		$this->renderSubmenu();
 	}
