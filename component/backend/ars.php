37c80aa4a67c4253e9253f7ac6331d127934275f
nikosdion <nikosdion@5fbba189-5f39-4019-a16b-6abf1605764a>
2012-06-09 16:56:54 +0000

    Use JPATH_LIBRARIES instead of a direct reference to the libraries path in the site's root
    git-svn-id: https://subversion.assembla.com/svn/akeeba-release-system/trunk@473 5fbba189-5f39-4019-a16b-6abf1605764a

--- component/backend/ars.php
+++ component/backend/ars.php
@@ -25,7 +25,7 @@ if(!version_compare($version, '5.2.7', '>='))
 }
 
 // Load FOF
-include_once JPATH_SITE.'/libraries/fof/include.php';
+include_once JPATH_LIBRARIES.'/fof/include.php';
 if(!defined('FOF_INCLUDED')) JError::raiseError ('500', 'Your Akeeba Release System installation is broken; please re-install. Alternatively, extract the installation archive and copy the fof directory inside your site\'s libraries directory.');
 
 FOFDispatcher::getTmpInstance('com_ars')->dispatch();
