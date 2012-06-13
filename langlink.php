71b9a83512377b2ed6b255d6e22574287cbfdce6
nikosdion <nikosdion@5fbba189-5f39-4019-a16b-6abf1605764a>
2012-06-13 17:33:28 +0000

    Moving around translation files
    git-svn-id: https://subversion.assembla.com/svn/akeeba-release-system/trunk@506 5fbba189-5f39-4019-a16b-6abf1605764a

---
+++ langlink.php
@@ -0,0 +1,38 @@
+<?php
+function doTheHippyHippyShake($root, $target)
+{
+	foreach(new DirectoryIterator($root) as $oArea) {
+		if(!$oArea->isDir()) continue;
+		if($oArea->isDot()) continue;
+		$area = $oArea->getFilename();
+
+		$areaDir = $root.'/'.$area;
+
+		foreach(new DirectoryIterator($areaDir) as $oModule)
+		{
+			if(!$oModule->isDir()) continue;
+			if($oModule->isDot()) continue;
+			$module = $oModule->getFilename();
+
+			$moduleDir = $areaDir.'/'.$module;
+
+			$from = $target.'/'.$area.'/'.$module.'/en-GB';
+			$to = $moduleDir.'/language/en-GB';
+			
+			if(is_link($to)) {
+				if(!@unlink($to)) {
+					continue;
+				}
+			}
+			@symlink($from, $to);
+		}
+	}	
+}
+
+$root = dirname(__FILE__).'/modules';
+$target = dirname(__FILE__).'/translations/modules';
+doTheHippyHippyShake($root, $target);
+
+$root = dirname(__FILE__).'/plugins';
+$target = dirname(__FILE__).'/translations/plugins';
+doTheHippyHippyShake($root, $target);
