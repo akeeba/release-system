453cb651765b54ac44877e7f403993b36f9826f6
nikosdion <nikosdion@5fbba189-5f39-4019-a16b-6abf1605764a>
2012-07-03 18:34:57 +0000

    Back-end category editor crash
    git-svn-id: https://subversion.assembla.com/svn/akeeba-release-system/trunk@537 5fbba189-5f39-4019-a16b-6abf1605764a

---
+++ component/backend/views/category/view.html.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package AkeebaReleaseSystem
+ * @copyright Copyright (c)2010-2012 Nicholas K. Dionysopoulos
+ * @license GNU General Public License version 3, or later
+ */
+
+defined('_JEXEC') or die();
+
+class ArsViewCategory extends FOFViewHtml
+{
+}
