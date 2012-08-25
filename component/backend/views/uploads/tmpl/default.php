f7377748537251101cbcf8947c36ed5e608bf7fd
tobias.dev <tobias.dev@5fbba189-5f39-4019-a16b-6abf1605764a>
2012-08-25 13:48:51 +0000

    Backend with bootstrap
    git-svn-id: https://subversion.assembla.com/svn/akeeba-release-system/trunk@625 5fbba189-5f39-4019-a16b-6abf1605764a

--- component/backend/views/uploads/tmpl/default.php
+++ component/backend/views/uploads/tmpl/default.php
@@ -13,7 +13,10 @@ $this->loadHelper('select');
 FOFTemplateUtils::addCSS('media://com_ars/css/backend.css');
 ?>
 
-<form name="adminForm" id="adminForm" action="index.php" method="post">
+<div class="row-fluid">
+<div class="span12">
+
+<form name="adminForm" id="adminForm" action="index.php" method="post" class="form form-horizontal">
 	<input type="hidden" name="option" value="<?php echo JRequest::getCmd('option') ?>" />
 	<input type="hidden" name="view" value="<?php echo JRequest::getCmd('view') ?>" />
 	<input type="hidden" name="task" id="task" value="category" />
@@ -21,17 +24,18 @@ FOFTemplateUtils::addCSS('media://com_ars/css/backend.css');
 	<input type="hidden" name="file" id="file" value="" />
 	<input type="hidden" name="<?php echo JFactory::getSession()->getToken();?>" value="1" />
 
-	<fieldset id="category-selection">
-		<legend><?php echo JText::_('COM_ARS_COMMON_CATEGORY_SELECT_LABEL');?></legend>
+		<h3><?php echo JText::_('COM_ARS_COMMON_CATEGORY_SELECT_LABEL');?></h3>
 		
-		<?php echo ArsHelperSelect::categories($this->category, 'id', array('onchange'=>'document.forms.adminForm.submit()')) ?>
-		<input type="submit" value="<?php echo JText::_('JSEARCH_FILTER_SUBMIT') ?>" />
+		<?php echo ArsHelperSelect::categories($this->category, 'id', array('onchange'=>'document.forms.adminForm.submit()','class' => 'input-medium')) ?>
+		<input type="submit" class="btn btn-mini" value="<?php echo JText::_('JSEARCH_FILTER_SUBMIT') ?>" />
 		<?php if(!empty($this->folder)): ?>
 		<div class="clr"></div>
 		<br/>
 		<?php echo JText::_('LBL_SUBFOLDER_NAME') ?>
 		<span id="subfoldername"><?php echo $this->escape($this->folder); ?></span>
-		<?php endif; ?>		
-	</fieldset>
+		<?php endif; ?>
 	
 </form>
+
+</div>
+</div>
