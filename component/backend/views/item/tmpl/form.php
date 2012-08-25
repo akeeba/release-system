17abb05ca116eb9d27145b991e1db357a2a979f3
tobias.dev <tobias.dev@5fbba189-5f39-4019-a16b-6abf1605764a>
2012-08-25 13:41:44 +0000

    Backend with bootstrap
    git-svn-id: https://subversion.assembla.com/svn/akeeba-release-system/trunk@618 5fbba189-5f39-4019-a16b-6abf1605764a

--- component/backend/views/item/tmpl/form.php
+++ component/backend/views/item/tmpl/form.php
@@ -21,109 +21,134 @@ if($this->item->id == 0) {
 
 ?>
 
-<form name="adminForm" id="adminForm" action="index.php" method="post">
+<form name="adminForm" id="adminForm" action="index.php" method="post" class="form form-horizontal">
 	<input type="hidden" name="option" value="<?php echo JRequest::getCmd('option') ?>" />
 	<input type="hidden" name="view" value="<?php echo JRequest::getCmd('view') ?>" />
 	<input type="hidden" name="task" value="" />
 	<input type="hidden" name="id" value="<?php echo $this->item->id ?>" />
 	<input type="hidden" name="<?php echo JFactory::getSession()->getToken();?>" value="1" />
 
-	<fieldset>
-		<legend><?php echo JText::_('COM_ARS_RELEASE_BASIC_LABEL'); ?></legend>
-
-		<div class="editform-row">
-			<label for="release_id"><?php echo JText::_('LBL_ITEMS_RELEASE'); ?></label>
-			<?php echo ArsHelperSelect::releases($this->item->release_id, 'release_id') ?>
+<div class="row-fluid">
+	
+	<div class="span6">
+		<h3><?php echo JText::_('COM_ARS_RELEASE_BASIC_LABEL'); ?></h3>
+		
+		<div class="control-group">
+			<label for="release_id" class="control-label"><?php echo JText::_('LBL_ITEMS_RELEASE'); ?></label>
+			<div class="controls">
+				<?php echo ArsHelperSelect::releases($this->item->release_id, 'release_id') ?>
+			</div>
 		</div>
-		<div class="editform-row">
-			<label for="title"><?php echo JText::_('LBL_ITEMS_TITLE'); ?></label>
-			<input type="text" name="title" id="title" value="<?php echo $this->item->title ?>">
+		<div class="control-group">
+			<label for="title" class="control-label"><?php echo JText::_('LBL_ITEMS_TITLE'); ?></label>
+			<div class="controls">
+				<input type="text" name="title" id="title" value="<?php echo $this->item->title ?>">
+			</div>
 		</div>
-		<div class="editform-row">
-			<label for="alias">
-				<?php echo JText::_('JFIELD_ALIAS_LABEL'); ?>
-			</label>
-			<input type="text" name="alias" id="alias" value="<?php echo $this->item->alias ?>" >
+		<div class="control-group">
+			<label for="alias" class="control-label"><?php echo JText::_('JFIELD_ALIAS_LABEL'); ?></label>
+			<div class="controls">
+				<input type="text" name="alias" id="alias" value="<?php echo $this->item->alias ?>" >
+			</div>
 		</div>
-		<div class="editform-row">
-			<label for="type"><?php echo JText::_('LBL_ITEMS_TYPE'); ?></label>
-			<?php echo ArsHelperSelect::itemtypes($this->item->type, 'type', array('onchange'=>'onTypeChange();')) ?>
+		<div class="control-group">
+			<label for="type" class="control-label"><?php echo JText::_('LBL_ITEMS_TYPE'); ?></label>
+			<div class="controls">
+				<?php echo ArsHelperSelect::itemtypes($this->item->type, 'type', array('onchange'=>'onTypeChange();')) ?>
+			</div>
 		</div>
-		<div class="editform-row" id="row-file" <?php if($this->item->type != 'file'):?>style="display: none"<?php endif;?> >
-			<label for="filename"><?php echo JText::_('LBL_ITEMS_FILE'); ?></label>
-			<span id="filename-container">
-			<?php echo ArsHelperSelect::getfiles($this->item->filename, $this->item->release_id, $this->item->id, 'filename', array('onchange'=>'onFileChange();')) ?>
-			</span>
+		<div class="control-group">
+			<label for="filename" class="control-label"><?php echo JText::_('LBL_ITEMS_FILE'); ?></label>
+			<div class="controls">
+				<?php echo ArsHelperSelect::getfiles($this->item->filename, $this->item->release_id, $this->item->id, 'filename', array('onchange'=>'onFileChange();')) ?>
+			</div>
 		</div>
-		<div class="editform-row" id="row-url" <?php if($this->item->type != 'link'):?>style="display: none"<?php endif;?> >
-			<label for="url"><?php echo JText::_('LBL_ITEMS_LINK'); ?></label>
-			<input type="text" name="url" id="url" value="<?php echo $this->item->url ?>" onblur="onLinkBlur();">
+		<div class="control-group">
+			<label for="url" class="control-label"><?php echo JText::_('LBL_ITEMS_LINK'); ?></label>
+			<div class="controls">
+				<input type="text" name="url" id="url" value="<?php echo $this->item->url ?>" onblur="onLinkBlur();">
+			</div>
 		</div>
-		<div class="editform-row">
-			<label for="environments"><?php echo JText::_( 'LBL_ITEMS_ENVIRONMENTS' ); ?></label>
-			<span style="float: left"><?php echo ArsHelperSelect::environments($this->item->environments); ?></span>
+		<div class="control-group">
+			<label for="environments" class="control-label"><?php echo JText::_('LBL_ITEMS_ENVIRONMENTS'); ?></label>
+			<div class="controls">
+				<?php echo ArsHelperSelect::environments($this->item->environments); ?>
+			</div>
 		</div>
 		<div style="clear:left"></div>
-		<div class="editform-row">
-			<label for="filesize"><?php echo JText::_('LBL_ITEMS_FILESIZE'); ?></label>
-			<input type="text" name="filesize" id="filesize" value="<?php echo $this->item->filesize ?>" >
+		
+		<div class="control-group">
+			<label for="filesize" class="control-label"><?php echo JText::_('LBL_ITEMS_FILESIZE'); ?></label>
+			<div class="controls">
+				<input type="text" name="filesize" id="filesize" value="<?php echo $this->item->filesize ?>" >
+			</div>
 		</div>
-		<div class="editform-row">
-			<label for="md5"><?php echo JText::_('LBL_ITEMS_MD5'); ?></label>
-			<input type="text" name="md5" id="md5" value="<?php echo $this->item->md5 ?>" >
+		<div class="control-group">
+			<label for="md5" class="control-label"><?php echo JText::_('LBL_ITEMS_MD5'); ?></label>
+			<div class="controls">
+				<input type="text" name="md5" id="md5" value="<?php echo $this->item->md5 ?>" >
+			</div>
 		</div>
-		<div class="editform-row">
-			<label for="sha1"><?php echo JText::_('LBL_ITEMS_SHA1'); ?></label>
-			<input type="text" name="sha1" id="sha1" value="<?php echo $this->item->sha1 ?>" >
+		<div class="control-group">
+			<label for="sha1" class="control-label"><?php echo JText::_('LBL_ITEMS_SHA1'); ?></label>
+			<div class="controls">
+				<input type="text" name="sha1" id="sha1" value="<?php echo $this->item->sha1 ?>" >
+			</div>
 		</div>
-		<div class="editform-row">
-			<label for="hits">
-				<?php echo JText::_('JGLOBAL_HITS'); ?>
-			</label>
-			<input type="text" name="hits" id="hits" value="<?php echo $this->item->hits ?>">
+		<div class="control-group">
+			<label for="hits" class="control-label"><?php echo JText::_('JGLOBAL_HITS'); ?></label>
+			<div class="controls">
+				<input type="text" name="hits" id="hits" value="<?php echo $this->item->hits ?>">
+			</div>
 		</div>
-		<div class="editform-row">
-			<label for="published">
-				<?php echo JText::_('JPUBLISHED'); ?>
-			</label>
-			<div>
+		<div class="control-group">
+			<label for="published" class="control-label"><?php echo JText::_('JPUBLISHED'); ?></label>
+			<div class="controls">
 				<?php echo JHTML::_('select.booleanlist', 'published', null, $this->item->published); ?>
 			</div>
 		</div>
-		<div class="editform-row editform-row-noheight">
-			<label for="access">
-				<?php echo JText::_('JFIELD_ACCESS_LABEL'); ?>
-			</label>
-			<?php echo JHTML::_('list.accesslevel', $this->item); ?>
+		<div class="control-group">
+			<label for="access" class="control-label"><?php echo JText::_('JFIELD_ACCESS_LABEL'); ?></label>
+			<div class="controls">
+				<?php echo JHTML::_('list.accesslevel', $this->item); ?>
+			</div>
 		</div>
 		<?php if(ArsHelperFiltering::hasAkeebaSubs()): ?>
-		<div class="editform-row editform-row-noheight">
-			<label for="groups"><?php echo JText::_('COM_ARS_COMMON_CATEGORIES_GROUPS_AKEEBA_LABEL'); ?></label>
-			<?php echo ArsHelperSelect::akeebasubsgroups($this->item->groups, 'groups') ?>
+		<div class="control-group">
+			<label for="groups" class="control-label"><?php echo JText::_('COM_ARS_COMMON_CATEGORIES_GROUPS_AKEEBA_LABEL'); ?></label>
+			<div class="controls">
+				<?php echo ArsHelperSelect::akeebasubsgroups($this->item->groups, 'groups') ?>
+			</div>
 		</div>
 		<?php elseif(defined('PAYPLANS_LOADED')): ?>
-		<div class="editform-row editform-row-noheight">
-			<label for="groups"><?php echo JText::_('COM_ARS_COMMON_CATEGORIES_GROUPS_PAYPLANS_LABEL'); ?></label>
-			<?php echo PayplansHtml::_('plans.edit', 'groups', $this->item->groups) ?>
+		<div class="control-group">
+			<label for="groups" class="control-label"><?php echo JText::_('COM_ARS_COMMON_CATEGORIES_GROUPS_PAYPLANS_LABEL'); ?></label>
+			<div class="controls">
+				<?php echo PayplansHtml::_('plans.edit', 'groups', $this->item->groups) ?>
+			</div>
 		</div>
 		<?php endif; ?>
-		<div class="editform-row">
-			<label for="updatestream"><?php echo JText::_('LBL_ITEMS_UPDATESTREAM'); ?></label>
-			<?php echo ArsHelperSelect::updatestreams($this->item->updatestream, 'updatestream') ?>
+		<div class="control-group">
+			<label for="updatestream" class="control-label"><?php echo JText::_('LBL_ITEMS_UPDATESTREAM'); ?></label>
+			<div class="controls">
+				<?php echo ArsHelperSelect::updatestreams($this->item->updatestream, 'updatestream') ?>
+			</div>
 		</div>
-		
-		<div class="editform-row editform-row-noheight">
-			<label for="language"><?php echo JText::_('JFIELD_LANGUAGE_LABEL'); ?></label>
-			<?php echo ArsHelperSelect::languages($this->item->language, 'language') ?>
+		<div class="control-group">
+			<label for="language" class="control-label"><?php echo JText::_('JFIELD_LANGUAGE_LABEL'); ?></label>
+			<div class="controls">
+				<?php echo ArsHelperSelect::languages($this->item->language, 'language') ?>
+			</div>
 		</div>
 		<div style="clear:left"></div>
 
-	</fieldset>
-
-	<fieldset>
-		<legend><?php echo JText::_('LBL_ARS_ITEMS_DESCRIPTION'); ?></legend>
-		<?php echo $editor->display( 'description',  $this->item->description, '600', '350', '60', '20', array() ) ; ?>
-	</fieldset>
+	</div>
+	<div class="span6">
+		
+		<h3><?php echo JText::_('LBL_ARS_ITEMS_DESCRIPTION'); ?></h3>
+		<?php echo $editor->display( 'description',  $this->item->description, '97%', '350', '60', '20', array() ) ; ?>
+		
+	</div>
 </form>
 
 <script type="text/javascript">
