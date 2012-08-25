1cbb9a2d91190adad4e66f492a75c593f60c23b0
tobias.dev <tobias.dev@5fbba189-5f39-4019-a16b-6abf1605764a>
2012-08-25 13:28:57 +0000

    Backend with bootstrap
    git-svn-id: https://subversion.assembla.com/svn/akeeba-release-system/trunk@610 5fbba189-5f39-4019-a16b-6abf1605764a

--- component/backend/views/autodesc/tmpl/form.php
+++ component/backend/views/autodesc/tmpl/form.php
@@ -15,46 +15,55 @@ FOFTemplateUtils::addCSS('media://com_ars/css/backend.css');
 
 ?>
 
-<form name="adminForm" id="adminForm" action="index.php" method="post">
+<form name="adminForm" id="adminForm" action="index.php" method="post" class="form form-horizontal">
 	<input type="hidden" name="option" value="<?php echo JRequest::getCmd('option') ?>" />
 	<input type="hidden" name="view" value="<?php echo JRequest::getCmd('view') ?>" />
 	<input type="hidden" name="task" value="" />
 	<input type="hidden" name="id" value="<?php echo $this->item->id ?>" />
 	<input type="hidden" name="<?php echo JFactory::getSession()->getToken();?>" value="1" />
 
-	<fieldset>
-		<legend><?php echo JText::_('LBL_ARS_AUTODESC_BASIC'); ?></legend>
+<div class="row-fluid">
+	
+	<div class="span6">
+		<h3><?php echo JText::_('LBL_ARS_AUTODESC_BASIC'); ?></h3>
 
-		<div class="editform-row">
-			<label for="category"><?php echo JText::_('LBL_AUTODESC_CATEGORY'); ?></label>
-			<?php echo ArsHelperSelect::categories($this->item->category, 'category') ?>
+		<div class="control-group">
+			<label for="category" class="control-label"><?php echo JText::_('LBL_AUTODESC_CATEGORY'); ?></label>
+			<div class="controls">
+				<?php echo ArsHelperSelect::categories($this->item->category, 'category') ?>
+			</div>
 		</div>
-		<div class="editform-row">
-			<label for="packname"><?php echo JText::_('LBL_AUTODESC_PACKNAME'); ?></label>
-			<input type="text" name="packname" id="packname" value="<?php echo $this->item->packname ?>">
+		<div class="control-group">
+			<label for="packname" class="control-label"><?php echo JText::_('LBL_AUTODESC_PACKNAME'); ?></label>
+			<div class="controls">
+				<input type="text" name="packname" id="packname" value="<?php echo $this->item->packname ?>">
+			</div>
 		</div>
-		<div class="editform-row">
-			<label for="title"><?php echo JText::_('LBL_AUTODESC_TITLE'); ?></label>
-			<input type="text" name="title" id="title" value="<?php echo $this->item->title ?>">
+		<div class="control-group">
+			<label for="title" class="control-label"><?php echo JText::_('LBL_AUTODESC_TITLE'); ?></label>
+			<div class="controls">
+				<input type="text" name="title" id="title" value="<?php echo $this->item->title ?>">
+			</div>
 		</div>
-		<div class="editform-row">
-			<label for="environments"><?php echo JText::_('LBL_ITEMS_ENVIRONMENTS'); ?></label>
-			<span style="float: left"><?php echo ArsHelperSelect::environments($this->item->environments); ?></span>
+		<div class="control-group">
+			<label for="environments" class="control-label"><?php echo JText::_('LBL_ITEMS_ENVIRONMENTS'); ?></label>
+			<div class="controls">
+				<?php echo ArsHelperSelect::environments($this->item->environments); ?>
+			</div>
 		</div>
 		<div style="clear:both"></div>
-		<div class="editform-row">
-			<label for="published">
-				<?php echo JText::_('JPUBLISHED'); ?>
-			</label>
-			<div>
+		<div class="control-group">
+			<label for="published" class="control-label"><?php echo JText::_('JPUBLISHED'); ?></label>
+			<div class="controls">
 				<?php echo JHTML::_('select.booleanlist', 'published', null, $this->item->published); ?>
 			</div>
 		</div>
-		<div style="clear:left"></div>
-	</fieldset>
+	</div>
 
-	<fieldset>
-		<legend><?php echo JText::_('LBL_AUTODESC_DESCRIPTION'); ?></legend>
-		<?php echo $editor->display( 'description',  $this->item->description, '600', '350', '60', '20', array() ) ; ?>
-	</fieldset>
+	<div class="span6">
+		<h3><?php echo JText::_('LBL_AUTODESC_DESCRIPTION'); ?></h3>
+		<?php echo $editor->display( 'description',  $this->item->description, '97%', '350', '60', '20', array() ) ; ?>
+	</div>
+	
+</div>
 </form>
