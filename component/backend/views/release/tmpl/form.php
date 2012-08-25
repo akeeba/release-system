1c794ba4ae402978980fe70dfd3d6c8a2d3bcb63
tobias.dev <tobias.dev@5fbba189-5f39-4019-a16b-6abf1605764a>
2012-08-25 13:45:16 +0000

    Backend with bootstrap
    git-svn-id: https://subversion.assembla.com/svn/akeeba-release-system/trunk@621 5fbba189-5f39-4019-a16b-6abf1605764a

--- component/backend/views/release/tmpl/form.php
+++ component/backend/views/release/tmpl/form.php
@@ -22,87 +22,109 @@ if($this->item->id == 0) {
 
 ?>
 
-<form name="adminForm" id="adminForm" action="index.php" method="post">
+<form name="adminForm" id="adminForm" action="index.php" method="post" class="form form-horizontal">
 	<input type="hidden" name="option" value="<?php echo JRequest::getCmd('option') ?>" />
 	<input type="hidden" name="view" value="<?php echo JRequest::getCmd('view') ?>" />
 	<input type="hidden" name="task" value="" />
 	<input type="hidden" name="id" value="<?php echo $this->item->id ?>" />
 	<input type="hidden" name="<?php echo JFactory::getSession()->getToken();?>" value="1" />
 
-	<fieldset>
-		<legend><?php echo JText::_('COM_ARS_RELEASE_BASIC_LABEL'); ?></legend>
+<div class="row-fluid">
+	
+	<div class="span6">
+		<h3><?php echo JText::_('COM_ARS_RELEASE_BASIC_LABEL'); ?></h3>
 
-		<div class="editform-row">
-			<label for="category_id"><?php echo JText::_('COM_ARS_RELEASES_FIELD_CATEGORY'); ?></label>
-			<?php echo ArsHelperSelect::categories($this->item->category_id, 'category_id') ?>
+		<div class="control-group">
+			<label for="category_id" class="control-label"><?php echo JText::_('COM_ARS_RELEASES_FIELD_CATEGORY'); ?></label>
+			<div class="controls">
+				<?php echo ArsHelperSelect::categories($this->item->category_id, 'category_id') ?>
+			</div>
 		</div>
-		<div class="editform-row">
-			<label for="version"><?php echo JText::_('COM_ARS_RELEASES_FIELD_VERSION'); ?></label>
-			<input type="text" name="version" id="version" value="<?php echo $this->item->version ?>">
+		<div class="control-group">
+			<label for="version" class="control-label"><?php echo JText::_('COM_ARS_RELEASES_FIELD_VERSION'); ?></label>
+			<div class="controls">
+				<input type="text" name="version" id="version" value="<?php echo $this->item->version ?>">
+			</div>
 		</div>
-		<div class="editform-row">
-			<label for="alias">
-				<?php echo JText::_('JFIELD_ALIAS_LABEL'); ?>
-			</label>
-			<input type="text" name="alias" id="alias" value="<?php echo $this->item->alias ?>">
+		<div class="control-group">
+			<label for="alias" class="control-label"><?php echo JText::_('JFIELD_ALIAS_LABEL'); ?></label>
+			<div class="controls">
+				<input type="text" name="alias" id="alias" value="<?php echo $this->item->alias ?>">
+			</div>
 		</div>
-		<div class="editform-row">
-			<label for="maturity"><?php echo JText::_('COM_ARS_RELEASES_FIELD_MATURITY'); ?></label>
-			<?php echo ArsHelperSelect::maturities($this->item->maturity, 'maturity') ?>
+		<div class="control-group">
+			<label for="maturity" class="control-label"><?php echo JText::_('JFIELD_ALIAS_LABEL'); ?></label>
+			<div class="controls">
+				<?php echo ArsHelperSelect::maturities($this->item->maturity, 'maturity') ?>
+			</div>
 		</div>
-		<div class="editform-row">
-			<label for="hits">
-				<?php echo JText::_('JGLOBAL_HITS'); ?>
-			</label>
-			<input type="text" name="hits" id="hits" value="<?php echo $this->item->hits ?>">
+		<div class="control-group">
+			<label for="hits" class="control-label"><?php echo JText::_('JGLOBAL_HITS'); ?></label>
+			<div class="controls">
+				<input type="text" name="hits" id="hits" value="<?php echo $this->item->hits ?>">
+			</div>
 		</div>
-		<div class="editform-row">
-			<label for="published">
-				<?php echo JText::_('JPUBLISHED'); ?>
-			</label>
-			<div>
+	
+	</div>
+	<div class="span6">
+		
+		<div class="control-group">
+			<label for="published" class="control-label"><?php echo JText::_('JPUBLISHED'); ?></label>
+			<div class="controls">
 				<?php echo JHTML::_('select.booleanlist', 'published', null, $this->item->published); ?>
 			</div>
 		</div>
-		<div class="editform-row editform-row-noheight">
-			<label for="access">
-				<?php echo JText::_('JFIELD_ACCESS_LABEL'); ?>
-			</label>
-			<?php echo JHTML::_('list.accesslevel', $this->item); ?>
+		<div class="control-group">
+			<label for="access" class="control-label"><?php echo JText::_('JFIELD_ACCESS_LABEL'); ?></label>
+			<div class="controls">
+				<?php echo JHTML::_('list.accesslevel', $this->item); ?>
+			</div>
 		</div>
 		<?php if(ArsHelperFiltering::hasAkeebaSubs()): ?>
-		<div class="editform-row editform-row-noheight">
-			<label for="groups"><?php echo JText::_('COM_ARS_COMMON_CATEGORIES_GROUPS_AKEEBA_LABEL'); ?></label>
-			<?php echo ArsHelperSelect::akeebasubsgroups($this->item->groups, 'groups') ?>
+		<div class="control-group">
+			<label for="groups" class="control-label"><?php echo JText::_('COM_ARS_COMMON_CATEGORIES_GROUPS_AKEEBA_LABEL'); ?></label>
+			<div class="controls">
+				<?php echo ArsHelperSelect::akeebasubsgroups($this->item->groups, 'groups') ?>
+			</div>
 		</div>
 		<?php elseif(defined('PAYPLANS_LOADED')): ?>
-		<div class="editform-row editform-row-noheight">
-			<label for="groups"><?php echo JText::_('COM_ARS_COMMON_CATEGORIES_GROUPS_PAYPLANS_LABEL'); ?></label>
-			<?php echo PayplansHtml::_('plans.edit', 'groups', $this->item->groups) ?>
+		<div class="control-group">
+			<label for="groups" class="control-label"><?php echo JText::_('COM_ARS_COMMON_CATEGORIES_GROUPS_PAYPLANS_LABEL'); ?></label>
+			<div class="controls">
+				<?php echo PayplansHtml::_('plans.edit', 'groups', $this->item->groups) ?>
+			</div>
 		</div>
 		<?php endif; ?>
 		<div style="clear:left"></div>
-		<div class="editform-row">
-			<label for="created"><?php echo JText::_('COM_ARS_RELEASES_FIELD_RELEASED'); ?></label>
-			<div>
+		<div class="control-group">
+			<label for="created" class="control-label"><?php echo JText::_('COM_ARS_RELEASES_FIELD_RELEASED'); ?></label>
+			<div class="controls">
 				<?php echo JHTML::_('calendar', $this->item->created, 'created', 'created'); ?>
 			</div>
 		</div>
 		
-		<div class="editform-row editform-row-noheight">
-			<label for="language"><?php echo JText::_('JFIELD_LANGUAGE_LABEL'); ?></label>
-			<?php echo ArsHelperSelect::languages($this->item->language, 'language') ?>
+		<div class="control-group">
+			<label for="language" class="control-label"><?php echo JText::_('JFIELD_LANGUAGE_LABEL'); ?></label>
+			<div class="controls">
+				<?php echo ArsHelperSelect::languages($this->item->language, 'language') ?>
+			</div>
 		</div>
-		<div style="clear:left"></div>
 
-	</fieldset>
-
-	<fieldset>
-		<legend><?php echo JText::_('COM_ARS_RELEASE_DESCRIPTION_LABEL'); ?></legend>
-		<?php echo $editor->display( 'description',  $this->item->description, '600', '350', '60', '20', array() ) ; ?>
-	</fieldset>
-	<fieldset>
-		<legend><?php echo JText::_('COM_ARS_RELEASE_NOTES_LABEL'); ?></legend>
-		<?php echo $editor->display( 'notes',  $this->item->notes, '600', '350', '60', '20', array() ) ; ?>
-	</fieldset>
+	</div>
+	
+</div>
+<hr/>
+<div class="row-fluid">
+	
+	<div class="span6">
+		<h3><?php echo JText::_('COM_ARS_RELEASE_DESCRIPTION_LABEL'); ?></h3>
+		<?php echo $editor->display( 'description',  $this->item->description, '97%', '350', '60', '20', array() ) ; ?>
+	</div>
+	
+	<div class="span6">
+		<h3><?php echo JText::_('COM_ARS_RELEASE_NOTES_LABEL'); ?></h3>
+		<?php echo $editor->display( 'notes',  $this->item->notes, '97%', '350', '60', '20', array() ) ; ?>
+	</div>
+	
+</div>
 </form>
