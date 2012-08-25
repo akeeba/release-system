feed0bb60c87858d62d80d54f1b3c6d94c2fbb1f
tobias.dev <tobias.dev@5fbba189-5f39-4019-a16b-6abf1605764a>
2012-08-25 13:37:59 +0000

    Backend with bootstrap
    git-svn-id: https://subversion.assembla.com/svn/akeeba-release-system/trunk@615 5fbba189-5f39-4019-a16b-6abf1605764a

--- component/backend/views/environment/tmpl/form.php
+++ component/backend/views/environment/tmpl/form.php
@@ -14,27 +14,37 @@ $this->loadHelper('select');
 FOFTemplateUtils::addCSS('media://com_ars/css/backend.css');
 ?>
 
-<form name="adminForm" id="adminForm" action="index.php" method="post">
+<form name="adminForm" id="adminForm" action="index.php" method="post" class="form form-horizontal">
 	<input type="hidden" name="option" value="<?php echo JRequest::getCmd('option') ?>" />
 	<input type="hidden" name="view" value="<?php echo JRequest::getCmd('view') ?>" />
 	<input type="hidden" name="task" value="" />
 	<input type="hidden" name="id" value="<?php echo $this->item->id ?>" />
 	<input type="hidden" name="<?php echo JFactory::getSession()->getToken();?>" value="1" />
-
-	<fieldset>
-		<legend><?php echo JText::_('COM_ARS_CATEGORY_BASIC_LABEL'); ?></legend>
+	
+<div class="row-fluid">
+	
+	<div class="span12">
+		<h3><?php echo JText::_('COM_ARS_CATEGORY_BASIC_LABEL'); ?></h3>
 		
-		<div class="editform-row">
-			<label for="title"><?php echo JText::_('LBL_ENVIRONMENT_TITLE'); ?></label>
-			<input type="text" name="title" id="title" value="<?php echo $this->item->title ?>">
+		<div class="control-group">
+			<label for="title" class="control-label"><?php echo JText::_('LBL_ENVIRONMENT_TITLE'); ?></label>
+			<div class="controls">
+				<input type="text" name="title" id="title" value="<?php echo $this->item->title ?>">
+			</div>
 		</div>
-		<div class="editform-row">
-			<label for="xmltitle"><?php echo JText::_('LBL_ENVIRONMENT_XMLTITLE'); ?></label>
-			<input type="text" name="xmltitle" id="xmltitle" value="<?php echo $this->item->xmltitle ?>" title="<?php echo JText::_('LBL_ENVIRONMENT_XMLTITLE_TIP'); ?>">
+		<div class="control-group">
+			<label for="xmltitle" class="control-label"><?php echo JText::_('LBL_ENVIRONMENT_XMLTITLE'); ?></label>
+			<div class="controls">
+				<input type="text" name="xmltitle" id="xmltitle" value="<?php echo $this->item->xmltitle ?>" title="<?php echo JText::_('LBL_ENVIRONMENT_XMLTITLE_TIP'); ?>">
+			</div>
 		</div>
-		<div class="editform-row">
-			<label for="icon"><?php echo JText::_('LBL_ENVIRONMENT_ICON'); ?></label>
-			<?php echo ArsHelperSelect::environmenticons( $this->item->icon ) ?>
+		<div class="control-group">
+			<label for="icon" class="control-label"><?php echo JText::_('LBL_ENVIRONMENT_ICON'); ?></label>
+			<div class="controls">
+				<?php echo ArsHelperSelect::environmenticons( $this->item->icon ) ?>
+			</div>
 		</div>
-	</fieldset>
+	</div>
+	
+</div>
 </form>
