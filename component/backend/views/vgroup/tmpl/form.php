e5fe91b0ba014163dee726184825510994cf3c09
tobias.dev <tobias.dev@5fbba189-5f39-4019-a16b-6abf1605764a>
2012-08-25 13:50:48 +0000

    Backend with bootstrap
    git-svn-id: https://subversion.assembla.com/svn/akeeba-release-system/trunk@627 5fbba189-5f39-4019-a16b-6abf1605764a

--- component/backend/views/vgroup/tmpl/form.php
+++ component/backend/views/vgroup/tmpl/form.php
@@ -14,24 +14,30 @@ $this->loadHelper('select');
 FOFTemplateUtils::addCSS('media://com_ars/css/backend.css');
 ?>
 
-<form name="adminForm" id="adminForm" action="index.php" method="post">
+<form name="adminForm" id="adminForm" action="index.php" method="post" class="form form-horizontal">
 	<input type="hidden" name="option" value="<?php echo JRequest::getCmd('option') ?>" />
 	<input type="hidden" name="view" value="vgroups" />
 	<input type="hidden" name="task" value="" />
 	<input type="hidden" name="id" value="<?php echo $this->item->id ?>" />
 	<input type="hidden" name="<?php echo JFactory::getSession()->getToken();?>" value="1" />
-
-	<div class="editform-row">
-		<label for="title"><?php echo JText::_('LBL_VGROUPS_TITLE'); ?></label>
-		<input type="text" name="title" id="title" value="<?php echo $this->item->title ?>">
+	
+<div class="row-fluid">
+	
+	<div class="span12">
+		
+	<div class="control-group">
+		<label for="title" class="control-label"><?php echo JText::_('LBL_VGROUPS_TITLE'); ?></label>
+		<div class="controls">
+			<input type="text" name="title" id="title" value="<?php echo $this->item->title ?>">
+		</div>
 	</div>
-	<div class="editform-row">
-		<label for="published">
-			<?php echo JText::_('JPUBLISHED'); ?>
-		</label>
-		<div>
+	<div class="control-group">
+		<label for="published" class="control-label"><?php echo JText::_('JPUBLISHED'); ?></label>
+		<div class="controls">
 			<?php echo JHTML::_('select.booleanlist', 'published', null, $this->item->published); ?>
 		</div>
 	</div>
-	<div style="clear:left"></div>
+	</div>
+	
+</div>
 </form>
