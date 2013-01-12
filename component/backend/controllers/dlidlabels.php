3523ddf8dd64c6c1190cc5531f07208001fcf29e
nikosdion <nikosdion@5fbba189-5f39-4019-a16b-6abf1605764a>
2013-01-12 09:42:30 +0000

    Re #41 ArsControllerDlidlabels: onBeforeEdit / onBeforeRead / onBeforeApplySave: make sure row's user_id is current user ID or throw 403
    git-svn-id: https://subversion.assembla.com/svn/akeeba-release-system/trunk@715 5fbba189-5f39-4019-a16b-6abf1605764a

--- component/backend/controllers/dlidlabels.php
+++ component/backend/controllers/dlidlabels.php
@@ -9,6 +9,14 @@ defined('_JEXEC') or die();
 
 class ArsControllerDlidlabels extends FOFController
 {
+	/**
+	 * Executes a controller task. Overriden to make sure non-logged-in users
+	 * cannot create add-on Download IDs.
+	 * 
+	 * @param   string  $task  The task to execute.
+	 * 
+	 * @throws  Exception
+	 */
 	public function execute($task) {
 		list($isCLI, $isAdmin) = FOFDispatcher::isCliAdmin();
 		
@@ -23,5 +31,96 @@ class ArsControllerDlidlabels extends FOFController
 		}
 		
 		parent::execute($task);
+	}
+	
+	/**
+	 * Edit view permissions check. Overriden to make sure a user won't try
+	 * editing another user's add-on Download IDs.
+	 * 
+	 * @return boolean
+	 * 
+	 * @throws Exception
+	 */
+	protected function onBeforeEdit() {
+		$result = parent::onBeforeEdit();
+		
+		list($isCLI, $isAdmin) = FOFDispatcher::isCliAdmin();
+		
+		if (($result !== false) && !$isAdmin && !$isCLI)
+		{
+			$model = $this->getThisModel();
+			if(!$model->getId())
+			{
+				$model->setIDsFromRequest();
+			}
+			
+			$item = $model->getItem();
+			if ($item->user_id != JFactory::getUser()->id)
+			{
+				throw new Exception(JText::_('JLIB_APPLICATION_ERROR_ACCESS_FORBIDDEN'), 403);
+				return false;
+			}
+		}
+		
+		return ($result !== false);
+	}
+	
+	/**
+	 * Read view permissions check. Overriden to make sure a user won't try
+	 * reading another user's add-on Download IDs.
+	 * 
+	 * @return boolean
+	 * @throws Exception
+	 */
+	protected function onBeforeRead()
+	{
+		list($isCLI, $isAdmin) = FOFDispatcher::isCliAdmin();
+		
+		if (!$isAdmin && !$isCLI)
+		{
+			$model = $this->getThisModel();
+			if(!$model->getId())
+			{
+				$model->setIDsFromRequest();
+			}
+			
+			$item = $model->getItem();
+			if ($item->user_id != JFactory::getUser()->id)
+			{
+				throw new Exception(JText::_('JLIB_APPLICATION_ERROR_ACCESS_FORBIDDEN'), 403);
+				return false;
+			}
+		}
+
+		return true;
+	}
+	
+	protected function onBeforeApplySave(&$data) {
+		$result = parent::onBeforeApplySave($data);
+		
+		list($isCLI, $isAdmin) = FOFDispatcher::isCliAdmin();
+		if (($result !== false) && !$isAdmin && !$isCLI)
+		{
+			if (JFactory::getUser()->guest)
+			{
+				throw new Exception(JText::_('JLIB_APPLICATION_ERROR_ACCESS_FORBIDDEN'), 403);
+				return false;
+			}
+
+			$result = true;
+			
+			$user_id = JFactory::getUser()->id;
+			$myData = (array)$data;
+			if (isset($data['user_id']))
+			{
+				if ($data['user_id'] != $user_id)
+				{
+					throw new Exception(JText::_('JLIB_APPLICATION_ERROR_ACCESS_FORBIDDEN'), 403);
+					return false;
+				}
+			}
+		}
+		
+		return $result;
 	}
 }
