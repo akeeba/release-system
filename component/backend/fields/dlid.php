8efd811dabfe6b9346524395d9c7406f2ae81ba3
nikosdion <nikosdion@5fbba189-5f39-4019-a16b-6abf1605764a>
2013-01-12 09:40:57 +0000

    Re #41 Backend forms for addon Download IDs
    git-svn-id: https://subversion.assembla.com/svn/akeeba-release-system/trunk@708 5fbba189-5f39-4019-a16b-6abf1605764a

---
+++ component/backend/fields/dlid.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package    FrameworkOnFramework
+ * @copyright  Copyright (C) 2010 - 2012 Akeeba Ltd. All rights reserved.
+ * @license    GNU General Public License version 3 or later; see LICENSE.txt
+ */
+
+// Protect from unauthorized access
+defined('_JEXEC') or die();
+
+/**
+ * Form Field class for the FOF framework
+ * Shows the due date field, either as a calendar input or as a formatted due date field
+ *
+ * @since       2.0
+ */
+class FOFFormFieldDlid extends FOFFormFieldText
+{
+	public function getRepeatable() {
+		$db = JFactory::getDbo();
+		$query = $db->getQuery(true)
+			->select(array(
+				'md5(concat('.$db->qn('id').','.$db->qn('username').','.$db->qn('password').')) AS '.$db->qn('dlid')
+			))
+			->from($db->qn('#__users'))
+			->where($db->qn('id').' = '.$db->q($this->item->user_id));
+		$masterDlid = $db->loadResult();
+		
+		$this->value = md5($this->item->user_id . $this->item->label . $masterDlid);
+		
+		return parent::getRepeatable();
+	}
+}
