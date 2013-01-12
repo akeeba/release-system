efa942ca414e20828168366d619870bc653bba59
nikosdion <nikosdion@5fbba189-5f39-4019-a16b-6abf1605764a>
2013-01-12 09:40:07 +0000

    Re #41 Adding table to hold add-on download ID labels
    git-svn-id: https://subversion.assembla.com/svn/akeeba-release-system/trunk@705 5fbba189-5f39-4019-a16b-6abf1605764a

--- component/backend/sql/install/mysql/install.sql
+++ component/backend/sql/install/mysql/install.sql
@@ -139,4 +139,16 @@ CREATE TABLE IF NOT EXISTS `#__ars_environments` (
   `xmltitle` varchar(20) NOT NULL DEFAULT '1.0',
   `icon` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `#__ars_dlidlabels` (
+  `ars_dlidlabel_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `label` varchar(255) NOT NULL DEFAULT '',
+  `enabled` tinyint(3) NOT NULL DEFAULT '1',
+  `created_by` bigint(20) NOT NULL DEFAULT '0',
+  `created_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified_by` bigint(20) NOT NULL DEFAULT '0',
+  `modified_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`ars_dlidlabel_id`)
+) DEFAULT CHARSET=utf8;
