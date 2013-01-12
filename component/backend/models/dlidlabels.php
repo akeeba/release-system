b653a3c142309c27903ceb96e3c7a31fd5690d0f
nikosdion <nikosdion@5fbba189-5f39-4019-a16b-6abf1605764a>
2013-01-12 09:41:09 +0000

    Re #41 Backend model to handle searches
    git-svn-id: https://subversion.assembla.com/svn/akeeba-release-system/trunk@709 5fbba189-5f39-4019-a16b-6abf1605764a

---
+++ component/backend/models/dlidlabels.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package AkeebaReleaseSystem
+ * @copyright Copyright (c)2010-2013 Nicholas K. Dionysopoulos
+ * @license GNU General Public License version 3, or later
+ */
+
+defined('_JEXEC') or die();
+
+class ArsModelDlidlabels extends FOFModel
+{
+	public function buildQuery($overrideLimits = false)
+	{
+		$query = parent::buildQuery($overrideLimits);
+		
+		$fltUsername		= $this->getState('username', null, 'string');
+		if($fltUsername) {
+			$db = JFactory::getDbo();
+			$fltUsername = '%' . $fltUsername . '%';
+			$q = $db->getQuery(true)
+				->select(array(
+					$db->qn('id')
+				))->from($db->qn('#__users'))
+				->where($db->qn('username') . ' LIKE ' . $db->q($fltUsername), 'OR')
+				->where($db->qn('name') . ' LIKE ' . $db->q($fltUsername))
+				->where($db->qn('email') . ' LIKE ' . $db->q($fltUsername))
+			;
+			$db->setQuery($q);
+			$ids = $db->loadColumn();
+			if(!empty($ids)) {
+				$query->where($db->qn('user_id') . 'IN (' . implode(',', $ids) . ')');
+			} else {
+				$query->where($db->qn('user_id') . '=' . $db->q(0));
+			}
+		}
+
+		return $query;
+	}
+}
