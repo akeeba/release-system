2ae795eb2e2452564506267f9b1ac4d1b55fc359
nikosdion <nikosdion@5fbba189-5f39-4019-a16b-6abf1605764a>
2013-02-04 17:49:24 +0000

    Improved message about incompatibility with PHP 5.2.17 or earlier
    git-svn-id: https://subversion.assembla.com/svn/akeeba-release-system/trunk@733 5fbba189-5f39-4019-a16b-6abf1605764a

--- component/backend/ars.php
+++ component/backend/ars.php
@@ -21,7 +21,7 @@ if(defined('PHP_VERSION')) {
 // Old PHP version detected. EJECT! EJECT! EJECT!
 if(!version_compare($version, '5.3.0', '>='))
 {
-	return JError::raise(E_ERROR, 500, 'PHP versions 4.x, 5.0, 5.1 and 5.2.0-5.2.6 are no longer supported by Akeeba Release System.','The version of PHP used on your site is obsolete and contains known security vulenrabilities. Moreover, it is missing features required by Akeeba Release System to work properly or at all. Please ask your host to upgrade your server to the latest PHP 5.3 release. Thank you!');
+	return JError::raise(E_ERROR, 500, 'PHP versions 4.x, 5.0, 5.1 and 5.2 are no longer supported by Akeeba Release System.<br/><br/>The version of PHP used on your site is obsolete and contains known security vulenrabilities. Moreover, it is missing features required by Akeeba Release System to work properly or at all. Please ask your host to upgrade your server to the latest PHP 5.3 release. Thank you!');
 }
 
 // Load FOF
