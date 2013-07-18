61c5a9e4f59232c4093a48515c50ad5de0b53024
nikosdion <nikosdion@5fbba189-5f39-4019-a16b-6abf1605764a>
2013-07-18 12:04:17 +0000

    Clear FOF 2.1's table cache on installation/update
    git-svn-id: https://subversion.assembla.com/svn/akeeba-release-system/trunk@817 5fbba189-5f39-4019-a16b-6abf1605764a

--- component/script.ars.php
+++ component/script.ars.php
@@ -218,6 +218,17 @@ class Com_ArsInstallerScript
 
 		// Kill update site
 		$this->_killUpdateSite();
+
+		// Clear FOF's cache
+		if (!defined('FOF_INCLUDED'))
+		{
+			@include_once JPATH_LIBRARIES . '/fof/include.php';
+
+			if (defined('FOF_INCLUDED'))
+			{
+				FOFPlatform::getInstance()->clearCache();
+			}
+		}
 	}
 
 	/**
