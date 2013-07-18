b8b8b30dc43c48c66dec60de0371477e581a15cd
nikosdion <nikosdion@5fbba189-5f39-4019-a16b-6abf1605764a>
2013-07-18 20:51:21 +0000

    backend.css is loaded in the front-end
    git-svn-id: https://subversion.assembla.com/svn/akeeba-release-system/trunk@820 5fbba189-5f39-4019-a16b-6abf1605764a

--- component/frontend/dispatcher.php
+++ component/frontend/dispatcher.php
@@ -26,7 +26,6 @@ class ArsDispatcher extends FOFDispatcher
 			include_once JPATH_ROOT.'/media/akeeba_strapper/strapper.php';
 			AkeebaStrapper::bootstrap();
 			AkeebaStrapper::jQueryUI();
-			AkeebaStrapper::addCSSfile('media://com_ars/css/backend.css');
 
 			// Default to the "browses" view
 			$view = $this->input->getCmd('view',$this->defaultView);
