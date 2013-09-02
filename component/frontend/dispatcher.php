e9fac80625b79a94132693b1916e168d973aef7f
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2013-09-02 20:16:02 +0300

    JSON may be removed in some PHP 5.5 distributions because the JSON format specification's author and Linux distro makers are sizing up their dicks. We are caught amidst a dick-fight. Therefore even though we can't fix stupid we can at least try to work around it. For fuck's sake!

--- component/frontend/dispatcher.php
+++ component/frontend/dispatcher.php
@@ -13,36 +13,50 @@ defined('_JEXEC') or die();
 class ArsDispatcher extends FOFDispatcher
 {
 	public $defaultView = 'browse';
-
 	private $allowedViews = array(
-		'browses','categories','downloads','latests','releases','updates',
-		'items','dlidlabels'
+		'browses', 'categories', 'downloads', 'latests', 'releases', 'updates',
+		'items', 'dlidlabels'
 	);
 
-	public function onBeforeDispatch() {
+	public function onBeforeDispatch()
+	{
+		// You can't fix stupid… but you can try working around it
+		if ((!function_exists('json_encode')) || (!function_exists('json_decode')))
+		{
+			require_once JPATH_ADMINISTRATOR . '/components/' . $this->component . '/helpers/jsonlib.php';
+		}
+
 		$result = parent::onBeforeDispatch();
-		if($result) {
+
+		if ($result)
+		{
 			// Load Akeeba Strapper
-			include_once JPATH_ROOT.'/media/akeeba_strapper/strapper.php';
+			include_once JPATH_ROOT . '/media/akeeba_strapper/strapper.php';
 			AkeebaStrapper::bootstrap();
 			AkeebaStrapper::jQueryUI();
 
 			// Default to the "browses" view
-			$view = $this->input->getCmd('view',$this->defaultView);
-			if(empty($view) || ($view == 'cpanel')) {
+			$view = $this->input->getCmd('view', $this->defaultView);
+			if (empty($view) || ($view == 'cpanel'))
+			{
 				$view = 'browses';
 			}
 
 			// Set the view, if it's allowed
-			$this->input->set('view',$view);
-			if(!in_array(FOFInflector::pluralize($view), $this->allowedViews)) $result = false;
-
-			if( ($this->input->getCmd('format','html') == 'html') &&
-				(FOFInflector::pluralize($view) == 'items') ) {
+			$this->input->set('view', $view);
+			if (!in_array(FOFInflector::pluralize($view), $this->allowedViews))
 				$result = false;
-			} elseif(FOFInflector::pluralize($view) == 'items') {
+
+			if (($this->input->getCmd('format', 'html') == 'html') &&
+				(FOFInflector::pluralize($view) == 'items'))
+			{
+				$result = false;
+			}
+			elseif (FOFInflector::pluralize($view) == 'items')
+			{
 				// Require admin
-				if(!JFactory::getUser()->authorise('core.manage','com_ars')) {
+				if (!JFactory::getUser()->authorise('core.manage', 'com_ars'))
+				{
 					JError::raiseError(403, 'Forbidden');
 					$result = false;
 				}
@@ -51,4 +65,5 @@ class ArsDispatcher extends FOFDispatcher
 
 		return $result;
 	}
+
 }
