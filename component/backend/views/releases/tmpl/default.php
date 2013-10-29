bea2c4e277f965fb1e3e932151c95c857c493dd5
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2013-10-29 11:25:28 +0200

    ~ Improved back-end display in Joomla! 3

--- component/backend/views/releases/tmpl/default.php
+++ component/backend/views/releases/tmpl/default.php
@@ -7,7 +7,13 @@
 
 defined('_JEXEC') or die();
 
+JHtml::_('behavior.tooltip');
 JHtml::_('behavior.multiselect');
+if (version_compare(JVERSION, '3.0', 'gt'))
+{
+	JHtml::_('dropdown.init');
+	JHtml::_('formbehavior.chosen', 'select');
+}
 
 $base_folder = rtrim(JURI::base(), '/');
 if(substr($base_folder, -13) == 'administrator') $base_folder = rtrim(substr($base_folder, 0, -13), '/');
@@ -17,7 +23,37 @@ $this->loadHelper('select');
 FOFTemplateUtils::addCSS('media://com_ars/css/backend.css');
 
 $hasAjaxOrderingSupport = $this->hasAjaxOrderingSupport();
+
+$sortFields = array(
+	'id'			=> JText::_('JGRID_HEADING_ID'),
+	'ordering'		=> JText::_('JFIELD_ORDERING_LABEL'),
+	'category_id'	=> JText::_('COM_ARS_RELEASES_FIELD_CATEGORY'),
+	'version'		=> JText::_('COM_ARS_RELEASES_FIELD_VERSION'),
+	'maturity'		=> JText::_('COM_ARS_RELEASES_FIELD_MATURITY'),
+	'access'		=> JText::_('JFIELD_ACCESS_LABEL'),
+	'published'		=> JText::_('JPUBLISHED'),
+	'hits'			=> JText::_('JGLOBAL_HITS'),
+	'language' 		=> JText::_('JFIELD_LANGUAGE_LABEL'),
+);
 ?>
+
+<?php if (version_compare(JVERSION, '3.0', 'ge')): ?>
+	<script type="text/javascript">
+		Joomla.orderTable = function() {
+			table = document.getElementById("sortTable");
+			direction = document.getElementById("directionTable");
+			order = table.options[table.selectedIndex].value;
+			if (order != '$order')
+			{
+				dirn = 'asc';
+			}
+			else {
+				dirn = direction.options[direction.selectedIndex].value;
+			}
+			Joomla.tableOrdering(order, dirn);
+		}
+	</script>
+<?php endif; ?>
 
 <div class="row-fluid">
 <div class="span12">
@@ -31,7 +67,37 @@ $hasAjaxOrderingSupport = $this->hasAjaxOrderingSupport();
 	<input type="hidden" name="filter_order" id="filter_order" value="<?php echo $this->lists->order ?>" />
 	<input type="hidden" name="filter_order_Dir" id="filter_order_Dir" value="<?php echo $this->lists->order_Dir ?>" />
 	<input type="hidden" name="<?php echo JFactory::getSession()->getFormToken();?>" value="1" />
-<table class="table table-striped" id="itemsList">
+
+	<?php if(version_compare(JVERSION, '3.0', 'gt')): ?>
+		<div id="filter-bar" class="btn-toolbar">
+			<div class="btn-group pull-right hidden-phone">
+				<label for="limit" class="element-invisible"><?php echo JText::_('JFIELD_PLG_SEARCH_SEARCHLIMIT_DESC') ?></label>
+				<?php echo $this->getModel()->getPagination()->getLimitBox(); ?>
+			</div>
+			<?php
+			$asc_sel	= ($this->getLists()->order_Dir == 'asc') ? 'selected="selected"' : '';
+			$desc_sel	= ($this->getLists()->order_Dir == 'desc') ? 'selected="selected"' : '';
+			?>
+			<div class="btn-group pull-right hidden-phone">
+				<label for="directionTable" class="element-invisible"><?php echo JText::_('JFIELD_ORDERING_DESC') ?></label>
+				<select name="directionTable" id="directionTable" class="input-medium" onchange="Joomla.orderTable()">
+					<option value=""><?php echo JText::_('JFIELD_ORDERING_DESC') ?></option>
+					<option value="asc" <?php echo $asc_sel ?>><?php echo JText::_('JGLOBAL_ORDER_ASCENDING') ?></option>
+					<option value="desc" <?php echo $desc_sel ?>><?php echo JText::_('JGLOBAL_ORDER_DESCENDING') ?></option>
+				</select>
+			</div>
+			<div class="btn-group pull-right">
+				<label for="sortTable" class="element-invisible"><?php echo JText::_('JGLOBAL_SORT_BY') ?></label>
+				<select name="sortTable" id="sortTable" class="input-medium" onchange="Joomla.orderTable()">
+					<option value=""><?php echo JText::_('JGLOBAL_SORT_BY') ?></option>
+					<?php echo JHtml::_('select.options', $sortFields, 'value', 'text', $this->getLists()->order) ?>
+				</select>
+			</div>
+		</div>
+		<div class="clearfix"> </div>
+	<?php endif; ?>
+
+	<table class="table table-striped" id="itemsList">
 	<thead>
 		<tr>
 			<?php if($hasAjaxOrderingSupport !== false): ?>
