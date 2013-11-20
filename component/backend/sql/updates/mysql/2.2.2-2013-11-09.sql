a8673d7be8ce1d4842d6ecad1e6b73ebeb940f16
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2013-11-20 15:40:16 +0200

    For crying out loud, what is it with me and SQL?

--- component/backend/sql/updates/mysql/2.2.2-2013-11-09.sql
+++ component/backend/sql/updates/mysql/2.2.2-2013-11-09.sql
@@ -1,7 +1,7 @@
-ALTER TABLE `#__ars_categories` ADD `show_unauth_links` TINYINT NOT NULL DEFAULT '0' AFTER `access` ;
-ALTER TABLE `#__ars_releases` ADD `show_unauth_links` TINYINT NOT NULL DEFAULT '0' AFTER `access` ;
-ALTER TABLE `#__ars_items` ADD `show_unauth_links` TINYINT NOT NULL DEFAULT '0' AFTER `access` ;
+ALTER TABLE `#__ars_categories` ADD COLUMN `show_unauth_links` TINYINT NOT NULL DEFAULT '0' AFTER `access` ;
+ALTER TABLE `#__ars_releases` ADD COLUMN `show_unauth_links` TINYINT NOT NULL DEFAULT '0' AFTER `access` ;
+ALTER TABLE `#__ars_items` ADD COLUMN `show_unauth_links` TINYINT NOT NULL DEFAULT '0' AFTER `access` ;
 
-ALTER TABLE `#__ars_categories` ADD `redirect_unauth` VARCHAR( 255 ) NOT NULL AFTER `show_unauth_links`;
-ALTER TABLE `#__ars_releases` ADD `redirect_unauth` VARCHAR( 255 ) NOT NULL AFTER `show_unauth_links`;
-ALTER TABLE `#__ars_items` ADD `redirect_unauth` VARCHAR( 255 ) NOT NULL AFTER `show_unauth_links`;
+ALTER TABLE `#__ars_categories` ADD COLUMN `redirect_unauth` VARCHAR( 255 ) NOT NULL AFTER `show_unauth_links`;
+ALTER TABLE `#__ars_releases` ADD COLUMN `redirect_unauth` VARCHAR( 255 ) NOT NULL AFTER `show_unauth_links`;
+ALTER TABLE `#__ars_items` ADD COLUMN `redirect_unauth` VARCHAR( 255 ) NOT NULL AFTER `show_unauth_links`;
