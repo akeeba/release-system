6e5ae96bbfc134c4d4ca70f750fcb7e7adab37cd
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2014-05-24 11:52:33 +0300

    Revert "# [HIGH] All items were unpublished when an item was not found in a Bleeding edge category"
    This reverts commit a29fdb785d6f06cea18be60ce84c70b6d4bf0885.
    
    Conflicts:
    	CHANGELOG

--- component/frontend/models/bleedingedge.php
+++ component/frontend/models/bleedingedge.php
@@ -340,6 +340,7 @@ class ArsModelBleedingedge extends F0FModel
 					$table = F0FModel::getTmpInstance('Releases', 'ArsModel')
 						->getTable();
 					$table->save($data, 'category_id');
+					$this->checkFiles($table);
 				}
 			}
 		}
