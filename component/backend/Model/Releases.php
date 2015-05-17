8cd0f7445ff83317d135bc84597b7c785c91622d
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2015-05-17 17:25:15 +0300

    Porting to FOF 3
    Imported the Releases model

---
+++ component/backend/Model/Releases.php
@@ -0,0 +1,355 @@
+<?php
+/**
+ * @package   AkeebaReleaseSystem
+ * @copyright Copyright (c)2010-2015 Nicholas K. Dionysopoulos
+ * @license   GNU General Public License version 3, or later
+ */
+
+namespace Akeeba\ReleaseSystem\Admin\Model;
+
+defined('_JEXEC') or die;
+
+use FOF30\Container\Container;
+use FOF30\Model\DataModel;
+
+class Releases extends DataModel
+{
+	use Mixin\ImplodedArrays;
+	use Mixin\Assertions;
+
+	/**
+	 * Public constructor. Overrides the parent constructor.
+	 *
+	 * @see DataModel::__construct()
+	 *
+	 * @param   Container  $container  The configuration variables to this model
+	 * @param   array      $config     Configuration values for this model
+	 *
+	 * @throws \FOF30\Model\DataModel\Exception\NoTableColumns
+	 */
+	public function __construct(Container $container, array $config = array())
+	{
+		$config['tableName'] = '#__ars_releases';
+		$config['idFieldName'] = 'id';
+		$config['aliasFields'] = [
+			'slug' 	      => 'alias',
+			'enabled'     => 'published',
+			'created_on'  => 'created',
+			'modified_on' => 'modified',
+			'locked_on'   => 'checked_out_time',
+			'locked_by'   => 'checked_out',
+		];
+
+		parent::__construct($container, $config);
+
+		// Relations
+		$this->belongsTo('category', 'Categories', 'category_id', 'id');
+		$this->hasMany('items', 'Items', 'id', 'release_id');
+
+		// Behaviours
+		$this->addBehaviour('Filters');
+		$this->addBehaviour('RelationFilters');
+		$this->addBehaviour('Created');
+		$this->addBehaviour('Modified');
+
+		// Some filters we will have to handle programmatically so we need to exclude them from the behaviour
+		$this->blacklistFilters([
+			'category_id',
+			'language',
+		]);
+
+		// Defaults
+		$this->access = 1;
+		$this->maturity = 'alpha';
+		$this->language = '*';
+	}
+
+	/**
+	 * Implements custom filtering
+	 *
+	 * @param   \JDatabaseQuery  $query           The model query we're operating on
+	 * @param   bool             $overrideLimits  Are we told to override limits?
+	 *
+	 * @return  void
+	 */
+	protected function onBeforeBuildQuery(\JDatabaseQuery &$query, $overrideLimits = false)
+	{
+		$db = $this->getDbo();
+
+		$fltCategory = $this->getState('category', null, 'int');
+
+		if ($fltCategory > 0)
+		{
+			$query->where($db->qn('category_id') . ' = ' . $db->q($fltCategory));
+		}
+
+		$fltAccessUser = $this->getState('access_user', null, 'int');
+
+		if (!is_null($fltAccessUser))
+		{
+			$access_levels = \JFactory::getUser($fltAccessUser)->getAuthorisedViewLevels();
+			$access_levels = array_map(array($db, 'quote'), $access_levels);
+
+			// Filter this table
+			$query->where($db->qn('access') . ' IN (' . implode(',', $access_levels) . ')');
+
+			// Filter the categories table, too
+			$this->whereHas('category', function(\JDatabaseQuery $subQuery) use($access_levels, $db) {
+				$subQuery->where($db->qn('access') . ' IN (' . implode(',', $access_levels) . ')');
+			});
+		}
+
+		$fltNoBEUnpub = $this->getState('nobeunpub', null, 'int');
+
+		if ($fltNoBEUnpub)
+		{
+			// Filter this table
+			$query->where($db->qn('published') . ' != ' . $db->q(0));
+
+			// Filter the categories table, too
+			$this->whereHas('category', function(\JDatabaseQuery $subQuery) use($db) {
+				$subQuery->where('NOT(' . $db->qn('published') . ' = ' . $db->q('0') . ' AND ' .
+					$db->qn('type') . '=' . $db->q('bleedingedge') . ')');
+			});
+		}
+
+		$fltLanguage = $this->getState('language', null, 'cmd');
+		$fltLanguage2 = $this->getState('language2', null, 'string');
+
+		if (($fltLanguage != '*') && ($fltLanguage != ''))
+		{
+			// Filter this table
+			$query->where($db->qn('language') . ' IN (' . $db->q('*') . ',' . $db->q($fltLanguage) . ')');
+
+			// Filter the categories table, too
+			$this->whereHas('category', function(\JDatabaseQuery $subQuery) use($db, $fltLanguage) {
+				$subQuery->where($db->qn('language') . ' IN (' . $db->q('*') . ',' . $db->q($fltLanguage) . ')');
+			});
+		}
+		elseif ($fltLanguage2)
+		{
+			// Filter this table
+			$query->where($db->qn('language') . ' = ' . $db->q($fltLanguage2));
+
+			// Filter the categories table, too
+			$this->whereHas('category', function(\JDatabaseQuery $subQuery) use($db, $fltLanguage2) {
+				$subQuery->where($db->qn('language') . ' = ' . $db->q($fltLanguage2));
+			});
+		}
+
+		$fltMaturity = $this->getState('maturity', 'alpha', 'cmd');
+
+		switch ($fltMaturity)
+		{
+			case 'beta':
+				$query->where($db->qn('maturity') . ' IN (' . $db->q('beta'), ',' . $db->q('rc') . ',' . $db->q('stable') . ')');
+				break;
+			case 'rc':
+				$query->where($db->qn('maturity') . ' IN (' . $db->q('rc') . ',' . $db->q('stable') . ')');
+				break;
+			case 'stable':
+				$query->where($db->qn('maturity') . ' = ' . $db->q('stable'));
+				break;
+		}
+	}
+
+	/**
+	 * Change the ordering of the records of the table
+	 *
+	 * @param   string $where The WHERE clause of the SQL used to fetch the order
+	 *
+	 * @return  static  Self, for chaining
+	 *
+	 * @throws  \UnexpectedValueException
+	 */
+	public function reorder($where = '')
+	{
+		if (empty($where))
+		{
+			$where = $this->getReorderWhere();
+		}
+
+		return parent::reorder($where);
+	}
+
+	/**
+	 * Get the default WHERE clause for reordering items. Called by reorder().
+	 *
+	 * @return string
+	 */
+	private function getReorderWhere()
+	{
+		$where = array();
+
+		$fltCategory = $this->getState('category', null, 'int');
+		$fltPublished = $this->getState('published', null, 'cmd');
+
+		$db = $this->getDBO();
+
+		if ($fltCategory)
+		{
+			$where[] = $db->qn('category_id') . ' = ' . $db->q($fltCategory);
+		}
+
+		if ($fltPublished != '')
+		{
+			$where[] = $db->qn('published') . ' = ' . $db->q($fltPublished);
+		}
+
+		if (count($where))
+		{
+			return '(' . implode(') AND (', $where) . ')';
+		}
+		else
+		{
+			return '';
+		}
+	}
+
+	public function check()
+	{
+		$this->assertNotEmpty($this->category_id, 'COM_RELEASE_ERR_NEEDS_CATEGORY');
+
+		// Get some useful info
+		$db = $this->getDBO();
+		$query = $db->getQuery(true)
+					->select(array(
+						$db->qn('version'),
+						$db->qn('alias')
+					))->from($db->qn('#__ars_releases'))
+					->where($db->qn('category_id') . ' = ' . $db->q($this->category_id));
+
+		if ($this->id)
+		{
+			$query->where('NOT(' . $db->qn('id') . '=' . $db->q($this->id) . ')');
+		}
+
+		$db->setQuery($query);
+		$info = $db->loadAssocList();
+		$versions = array();
+		$aliases = array();
+
+		foreach ($info as $infoitem)
+		{
+			$versions[] = $infoitem['version'];
+			$aliases[] = $infoitem['alias'];
+		}
+
+		$this->assertNotEmpty($this->version, 'COM_RELEASE_ERR_NEEDS_VERSION');
+
+		$this->assertNotInArray($this->version, $versions, 'COM_RELEASE_ERR_NEEDS_VERSION_UNIQUE');
+
+		// If the alias is missing, auto-create a new one
+		if (!$this->alias)
+		{
+			\JLoader::import('joomla.filter.input');
+
+			// Get the category title
+			/** @var Categories $catModel */
+			$catModel = $this->container->factory->model('Categories')->tmpInstance();
+			$catItem = $catModel->find($this->category_id);
+
+			// Create a smart alias
+			$alias = strtolower($catItem->alias . '-' . $this->version);
+			$alias = str_replace(' ', '-', $alias);
+			$alias = str_replace('.', '-', $alias);
+			$this->alias = (string)preg_replace('/[^A-Z0-9_-]/i', '', $alias);
+		}
+
+		$this->assertNotEmpty($this->alias, 'COM_RELEASE_ERR_NEEDS_ALIAS');
+
+		$this->assertNotInArray($this->alias, $aliases, 'COM_RELEASE_ERR_NEEDS_ALIAS_UNIQUE');
+
+		// Automaticaly fix the maturity
+		if (!in_array($this->maturity, array('alpha', 'beta', 'rc', 'stable')))
+		{
+			$this->maturity = 'beta';
+		}
+
+		\JLoader::import('joomla.filter.filterinput');
+		$filter = \JFilterInput::getInstance(null, null, 1, 1);
+
+		// Filter the description using a safe HTML filter
+		if (!empty($this->description))
+		{
+			$this->description = $filter->clean($this->description);
+		}
+
+		// Filter the notes using a safe HTML filter
+		if (!empty($this->notes))
+		{
+			$this->notes = $filter->clean($this->notes);
+		}
+
+		// Set the access to registered if there are subscriptions defined
+		if (!empty($this->groups) && ($this->access == 1))
+		{
+			$this->access = 2;
+		}
+
+		if (empty($this->published) && ($this->published !== 0))
+		{
+			$this->published = 0;
+		}
+
+		return parent::check();
+	}
+
+	/**
+	 * Reorders the entire releases array on inserting a new object and sets the current ordering to 1
+	 *
+	 * @param   \stdClass  $dataObject
+	 *
+	 * @return  void
+	 */
+	protected function onBeforeCreate(&$dataObject)
+	{
+		$dataObject->ordering = 1;
+		$this->ordering = 1;
+
+		$db = $this->getDbo();
+
+		$query = $db->getQuery(true)
+					->update($db->qn('#__ars_releases'))
+					->set($db->qn('ordering') . ' = ' . $db->qn('ordering') . ' + ' . $db->q(1));
+
+		// Only update releases with the same category (as long as a category is – and it should be!) defined
+		if (isset($dataObject->category_id) && !empty($dataObject->category_id))
+		{
+			$query->where($db->qn('category_id') . ' = ' . $db->q($dataObject->category_id));
+		}
+
+		try
+		{
+			$db->setQuery($query)->execute();
+		}
+		catch (\Exception $e)
+		{
+			// Do not fail on database error
+		}
+	}
+
+	/**
+	 * Converts the loaded comma-separated list of subscription levels into an array
+	 *
+	 * @param   string  $value  The comma-separated list
+	 *
+	 * @return  array  The exploded array
+	 */
+	protected function getGroupsAttribute($value)
+	{
+		return $this->getAttributeForImplodedArray($value);
+	}
+
+	/**
+	 * Converts the array of subscription levels into a comma separated list
+	 *
+	 * @param   array  $value  The array of values
+	 *
+	 * @return  string  The imploded comma-separated list
+	 */
+	protected function setGroupsAttribute($value)
+	{
+		return $this->setAttributeForImplodedArray($value);
+	}
+}
