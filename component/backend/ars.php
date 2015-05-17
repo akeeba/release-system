8cacb00221267b516dfd9d9bba0e2516bb4893b8
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2015-05-17 16:06:00 +0300

    Porting to FOF 3
    Imported the AutoDescriptions model

--- component/backend/ars.php
+++ component/backend/ars.php
@@ -13,4 +13,8 @@ if (!defined('FOF30_INCLUDED') && !@include_once(JPATH_LIBRARIES . '/fof30/inclu
 	throw new RuntimeException('FOF 3.0 is not installed', 500);
 }
 
+//$model = FOF30\Container\Container::getInstance('com_ars')->factory->model('AutoDescriptions');
+//$xo = new FOF30\Utils\ModelTypeHints($model);
+//echo "<pre>" . $xo->getHints() . "</pre>"; die;
+
 FOF30\Container\Container::getInstance('com_ars')->dispatcher->dispatch();
