03ae070fc44b62f07ccb02a4e7eb1b8ef5409eda
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2015-05-22 18:24:15 +0300

    Porting to FOF 3
    Frontend entry point file

---
+++ component/frontend/ars.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package   AkeebaReleaseSystem
+ * @copyright Copyright (c)2010-2015 Nicholas K. Dionysopoulos
+ * @license   GNU General Public License version 3, or later
+ */
+
+// Protect from unauthorized access
+defined('_JEXEC') or die();
+
+if (!defined('FOF30_INCLUDED') && !@include_once(JPATH_LIBRARIES . '/fof30/include.php'))
+{
+	throw new RuntimeException('FOF 3.0 is not installed', 500);
+}
+
+FOF30\Container\Container::getInstance('com_ars')->dispatcher->dispatch();
