99943501f27b29ceb2050c3d4bb9ad56ce69d4ed
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2015-05-23 11:42:12 +0300

    Porting to FOF 3
    New release: if a category was selected in the browse page use it as the default on the new release

--- component/backend/Controller/Release.php
+++ component/backend/Controller/Release.php
@@ -9,6 +9,7 @@ namespace Akeeba\ReleaseSystem\Admin\Controller;
 
 defined('_JEXEC') or die;
 
+use Akeeba\ReleaseSystem\Admin\Model\Releases;
 use FOF30\Controller\DataController;
 
 class Release extends DataController
@@ -22,4 +23,41 @@ class Release extends DataController
 		}
 	}
 
+	protected function onBeforeAdd()
+	{
+		$this->defaultsForAdd = [
+			'category'  => 0,
+			'maturity'  => 'stable',
+			'access'    => 1,
+			'published' => 0,
+			'language'  => '*',
+		];
+
+		$category = $this->getModel()->getState('category_id', null, 'int');
+
+		if ($category)
+		{
+			$this->defaultsForAdd['category_id'] = $category;
+		}
+
+		if ($maturity = $this->getModel()->getState('maturity', 'stable', 'string'))
+		{
+			$this->defaultsForAdd['maturity'] = $maturity;
+		}
+
+		if ($access = $this->getModel()->getState('access', 1, 'int'))
+		{
+			$this->defaultsForAdd['access'] = $access;
+		}
+
+		if ($published = $this->getModel()->getState('published', 0, 'int'))
+		{
+			$this->defaultsForAdd['published'] = $published;
+		}
+
+		if ($language = $this->getModel()->getState('language', '*', 'string'))
+		{
+			$this->defaultsForAdd['language'] = $language;
+		}
+	}
 }
