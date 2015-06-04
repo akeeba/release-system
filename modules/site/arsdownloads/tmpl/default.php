76551f767fe393ca378bb8547f132d7d405f3391
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2015-06-04 23:23:42 +0300

    Migrating to FOF 3
    My Downloads site module

--- modules/site/arsdownloads/tmpl/default.php
+++ modules/site/arsdownloads/tmpl/default.php
@@ -16,14 +16,14 @@ defined('_JEXEC') or die();
 				<td width="200"><b><?php echo htmlentities($i->name) ?></b> <?php echo $i->version ?></td>
 				<td class="button4">
 					<a class="readon"
-					   href="<?php echo JRoute::_('index.php?option=com_ars&view=download&format=raw&id=' . $i->id) ?>">
-						<span>Download</span>
+					   href="<?php echo JRoute::_('index.php?option=com_ars&view=Item&format=raw&id=' . $i->id) ?>">
+						<span><?php echo JText::_('MOD_ARSDOWNLOADS_LBL_DOWNLOAD'); ?></span>
 					</a>
 				</td>
 				<td width="100">
 					<a class="readon"
-					   href="<?php echo JRoute::_('index.php?option=com_ars&view=release&id=' . $i->release_id) ?>">
-						<span>View all</span>
+					   href="<?php echo JRoute::_('index.php?option=com_ars&view=Items&release_id=' . $i->release_id) ?>">
+						<span><?php echo JText::_('MOD_ARSDOWNLOADS_LBL_VIEWALL') ?></span>
 					</a>
 				</td>
 			</tr>
