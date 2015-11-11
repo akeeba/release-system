c3d240171d768870a9a21122e00eab7515564055
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2015-11-11 16:12:47 +0200

    # [HIGH] Update streams did not have the correct environment version encoded in them

--- component/frontend/Model/Update.php
+++ component/frontend/Model/Update.php
@@ -115,7 +115,20 @@ class Update extends Model
 					->order($db->qn('r') . '.' . $db->qn('created') . ' DESC');
 		$db->setQuery($query);
 
-		return $db->loadObjectList();
+		$ret = $db->loadObjectList();
+
+		if (is_array($ret) && !empty($ret))
+		{
+			foreach ($ret as &$item)
+			{
+				$environments = $item->environments;
+				$environments = empty($environments) ? array() : json_decode($environments);
+				$environments = empty($environments) ? array() : $environments;
+				$item->environments = $environments;
+			}
+		}
+
+		return $ret;
 	}
 
 	public function getPublished($id)
