58ac18436b3f2db04f63c05dbb2a2b97928b91e2
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2016-05-22 18:49:23 +0200

    Show more information link for release in Items view

--- component/frontend/View/Items/tmpl/release.blade.php
+++ component/frontend/View/Items/tmpl/release.blade.php
@@ -28,6 +28,17 @@ $released = JFactory::getDate($item->created);
 	<p class="text-muted">
 		<strong>@lang('LBL_RELEASES_RELEASEDON')</strong>:
 		@jhtml('date', $released, JText::_('DATE_FORMAT_LC2'))
+
+		<button class="btn btn-link" type="button" data-toggle="collapse"
+				data-target="#ars-release-{{{ $item->id }}}-info" aria-expanded="false"
+				aria-controls="ars-release-{{{ $item->id }}}-info">
+			<span class="glyphicon glyphicon-info-sign"></span>
+			@lang('COM_ARS_RELEASES_MOREINFO')
+		</button>
 	</p>
 	<p>&nbsp;</p>
+
+	<div id="ars-release-{{{ $item->id }}}-info" class="well collapse">
+		{{ Format::preProcessMessage($item->notes, 'com_ars.release_notes') }}
+	</div>
 </div>
