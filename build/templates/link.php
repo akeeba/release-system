13d0925d79db379c423df66f4572b4c15e1a69ef
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2017-02-19 12:59:31 +0200

    Remove translations

--- build/templates/link.php
+++ build/templates/link.php
@@ -12,8 +12,4 @@ $symlink_folders = array(
 	'../buildfiles/buildlang'					=> 'build/buildlang',
 	'../buildfiles/phingext'					=> 'build/phingext',
 	'../buildfiles/tools'						=> 'build/tools',
-
-	# Component translation
-	'translations/component/backend/en-GB'		=> 'component/language/backend/en-GB',
-	'translations/component/frontend/en-GB'		=> 'component/language/frontend/en-GB',
 );
