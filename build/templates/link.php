51a8682393d488d893fe22d293a76c79be61dd4d
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2017-03-08 22:57:36 +0200

    Simplify build directory

--- build/templates/link.php
+++ build/templates/link.php
@@ -7,9 +7,4 @@ $symlink_files = array(
 );
 
 $symlink_folders = array(
-	# Build files
-	'../buildfiles/bin'							=> 'build/bin',
-	'../buildfiles/buildlang'					=> 'build/buildlang',
-	'../buildfiles/phingext'					=> 'build/phingext',
-	'../buildfiles/tools'						=> 'build/tools',
 );
