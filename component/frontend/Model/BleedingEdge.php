ab21f33a103674f6cdfdeb2e16539846a66f1bc8
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2017-12-30 15:05:51 +0200

    List of files not rendered for Amazon S3 categories
    Closes gh-128

--- component/frontend/Model/BleedingEdge.php
+++ component/frontend/Model/BleedingEdge.php
@@ -77,7 +77,7 @@ class BleedingEdge extends Model
 			// If it is stored on S3 make sure there are files stored with the given directory prefix
 			$check = substr($folder, 5);
 			$s3 = AmazonS3::getInstance();
-			$items = $s3->getBucket('', $check . '/');
+			$items = $s3->getBucket('', $check . '/', null, null, '\0', false);
 
 			if (empty($items))
 			{
@@ -182,7 +182,7 @@ class BleedingEdge extends Model
 					{
 						$check = substr($folder, 5);
 						$s3 = AmazonS3::getInstance();
-						$items = $s3->getBucket('', $check . '/');
+						$items = $s3->getBucket('', $check . '/', null, null, '\0', false);
 						$exists = !empty($items);
 					}
 					else
