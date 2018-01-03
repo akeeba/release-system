817952050c18b1a8dc2dbe8dfff43e3898c89bc9
Davide Tampellini <tampe125@gmail.com>
2018-01-03 15:49:07 +0100

    Updating copyright headers

--- component/backend/View/VisualGroups/tmpl/form.php
+++ component/backend/View/VisualGroups/tmpl/form.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package   AkeebaReleaseSystem
- * @copyright 2010-2017 Akeeba Ltd / Nicholas K. Dionysopoulos
+ * @copyright Copyright (c)2010-2018 Nicholas K. Dionysopoulos / Akeeba Ltd
  * @license   GNU General Public License version 3, or later
  */
 
