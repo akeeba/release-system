75bdd0ba12d96353db76250632f4e6389740fd21
Davide Tampellini <tampe125@gmail.com>
2018-01-22 10:32:38 +0100

    Removed XML form

--- component/script.com_ars.php
+++ component/script.com_ars.php
@@ -91,6 +91,7 @@ class Com_ArsInstallerScript extends \FOF30\Utils\InstallScript
 			'administrator/components/com_ars/View/Categories/tmpl/form.form.xml',
 			'administrator/components/com_ars/View/Categories/tmpl/form.default.xml',
             'administrator/components/com_ars/View/DownloadIDLabels/tmpl/form.default.xml',
+            'administrator/components/com_ars/View/DownloadIDLabels/tmpl/form.form.xml',
             'administrator/components/com_ars/View/DownloadIDLabels/tmpl/default_dlid.php',
             'administrator/components/com_ars/View/Environments/tmpl/form.default.xml',
             'administrator/components/com_ars/View/Environments/tmpl/form.form.xml',
