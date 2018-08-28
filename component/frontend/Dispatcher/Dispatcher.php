9f6456ffa35aaa88e4af9b0ce8ee417b9366b069
Michael Babker <michael.babker@gmail.com>
2018-08-27 20:18:21 -0500

    Check correct param values for loading FEF on the frontend

--- component/frontend/Dispatcher/Dispatcher.php
+++ component/frontend/Dispatcher/Dispatcher.php
@@ -49,8 +49,8 @@ class Dispatcher extends \FOF30\Dispatcher\Dispatcher
         $useFEF   = $this->container->params->get('load_fef', 3);
         $fefReset = $this->container->params->get('fef_reset', 3);
 
-        $this->container->renderer->setOption('load_fef', in_array($useFEF, [2,3]));
-        $this->container->renderer->setOption('fef_reset', in_array($fefReset, [2,3]));
+        $this->container->renderer->setOption('load_fef', in_array($useFEF, [1,3]));
+        $this->container->renderer->setOption('fef_reset', in_array($fefReset, [1,3]));
         $this->container->renderer->setOption('linkbar_style', 'classic');
 
 		// Load common CSS and JavaScript
