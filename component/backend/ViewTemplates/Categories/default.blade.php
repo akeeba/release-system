e2327b4f5fbf05bcabdc48457e04bc6b738f6e2b
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2019-08-16 16:23:47 +0300

    Convert view templates to Blade
    Categories

---
+++ component/backend/ViewTemplates/Categories/default.blade.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * @package   AkeebaReleaseSystem
+ * @copyright Copyright (c)2010-2019 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+use Akeeba\ReleaseSystem\Admin\Helper\Html;use Akeeba\ReleaseSystem\Admin\Helper\Select;use Akeeba\ReleaseSystem\Admin\Model\Categories;use FOF30\Utils\FEFHelper\Html as FEFHtml;use Joomla\CMS\HTML\HTMLHelper;use Joomla\CMS\Language\Text;
+
+/** @var $this \Akeeba\ReleaseSystem\Admin\View\Categories\Html */
+
+defined('_JEXEC') or die;
+
+?>
+@extends('admin:com_ars/Common/browse')
+
+@section('browse-filters')
+	<div class="akeeba-filter-element akeeba-form-group">
+		<?php echo Select::categoryType('type', $this->filters['type'], ['onchange' => 'document.adminForm.submit()'])?>
+	</div>
+
+	<div class="akeeba-filter-element akeeba-form-group">
+		<?php echo Select::accessLevel('access', $this->filters['access'], ['onchange' => 'document.adminForm.submit()']);?>
+	</div>
+
+	<div class="akeeba-filter-element akeeba-form-group">
+		{{ \FOF30\Utils\FEFHelper\BrowseView::publishedFilter('published', 'JPUBLISHED') }}
+	</div>
+
+	<div class="akeeba-filter-element akeeba-form-group">
+		<?php echo Select::languages('language', $this->filters['language'], ['onchange' => 'document.adminForm.submit()'])?>
+	</div>
+@stop
+
+@section('browse-table-header')
+	<tr>
+		<td width="8%">
+			@sortgrid('ordering', '<i class="icon-menu-2"></i>')
+		</td>
+		<th width="32">
+			@jhtml('FEFHelper.browse.checkall')
+		</th>
+		<th>
+			@sortgrid('title', 'COM_ARS_CATEGORIES_FIELD_TITLE')
+		</th>
+		<th>
+			@sortgrid('type', 'COM_ARS_CATEGORIES_FIELD_TYPE')
+		</th>
+		<th>
+			@sortgrid('access', 'JFIELD_ACCESS_LABEL')
+		</th>
+		<th width="8%">
+			@sortgrid('published', 'JPUBLISHED')
+		</th>
+		<th>
+			@sortgrid('language', 'JFIELD_LANGUAGE_LABEL')
+		</th>
+	</tr>
+@stop
+
+@section('browse-table-body-withrecords')
+	<?php
+	$i = 0;
+	/** @var Categories $row */
+	?>
+	@foreach($this->items as $row)
+		<?php
+		$enabled = $this->container->platform->getUser()->authorise('core.edit.state', 'com_ars')
+		?>
+		<tr>
+			<td>
+				@jhtml('FEFHelper.browse.order', 'ordering', $row->ordering)
+			</td>
+			<td>
+				@jhtml('FEFHelper.browse.id', ++$i, $row->getId())
+			</td>
+			<td>
+				<a href="index.php?option=com_ars&view=Category&id={{ $row->getId() }}">
+					{{{ $row->title }}}
+				</a>
+			</td>
+			<td>
+				@if ($row->type == 'normal')
+					@lang('COM_ARS_CATEGORIES_TYPE_NORMAL')
+				@else
+					@lang('COM_ARS_CATEGORIES_TYPE_BLEEDINGEDGE')
+				@endif
+			</td>
+			<td>
+				{{ Html::accessLevel($row->access) }}
+			</td>
+			<td>
+				@jhtml('FEFHelper.browse.published', $row->published, $i, '', $enabled)
+			</td>
+			<td>
+				{{ Html::language($row->language) }}
+			</td>
+		</tr>
+	@endforeach
+@stop
---
+++ component/backend/ViewTemplates/Categories/form.blade.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * @package   AkeebaReleaseSystem
+ * @copyright Copyright (c)2010-2019 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+use Akeeba\ReleaseSystem\Admin\Helper\Html;
+use Akeeba\ReleaseSystem\Admin\Helper\Select;
+use Joomla\CMS\Editor\Editor;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
+/** @var \Akeeba\ReleaseSystem\Admin\View\Categories\Html $this */
+
+defined('_JEXEC') or die;
+
+/** @var \Akeeba\ReleaseSystem\Admin\Model\Categories $item */
+$item = $this->getItem();
+?>
+
+@extends('admin:com_ars/Common/edit')
+
+@section('edit-form-body')
+    <div class="akeeba-container--50-50">
+        <div>
+            <h3>
+                @lang('COM_ARS_RELEASE_BASIC_LABEL')
+            </h3>
+            <div class="akeeba-form-group">
+                <label for="title">@lang('COM_ARS_CATEGORIES_FIELD_TITLE')</label>
+
+                <input type="text" name="title" id="title" value="{{{ $item->title }}}" />
+            </div>
+
+            <div class="akeeba-form-group">
+                <label for="alias">@lang('COM_ARS_CATEGORIES_FIELD_ALIAS')</label>
+
+                <input type="text" name="alias" id="alias" value="{{{ $item->alias }}}" />
+            </div>
+
+            <div class="akeeba-form-group">
+                <label for="type">@lang('COM_ARS_CATEGORIES_FIELD_TYPE')</label>
+
+                {{ Select::categoryType('type', $item->type) }}
+            </div>
+
+            <div class="akeeba-form-group">
+                <label for="directory">@lang('COM_ARS_CATEGORIES_FIELD_DIRECTORY')</label>
+
+                <input type="text" name="directory" id="directory" value="{{{ $item->directory }}}" />
+            </div>
+
+            <div class="akeeba-form-group">
+                <label for="published">@lang('JPUBLISHED')</label>
+
+                @jhtml('FEFHelper.select.booleanswitch', 'published', $item->published)
+            </div>
+
+            <div class="akeeba-form-group">
+                <label for="access">@lang('JFIELD_ACCESS_LABEL')</label>
+
+                {{ Select::accessLevel('access', $item->access) }}
+            </div>
+
+            <div class="akeeba-form-group">
+                <label for="show_unauth_links">@lang('COM_ARS_COMMON_SHOW_UNAUTH_LINKS')</label>
+
+                @jhtml('FEFHelper.select.booleanswitch', 'show_unauth_links', $item->show_unauth_links)
+            </div>
+
+            <div class="akeeba-form-group">
+                <label for="groups">@lang('COM_ARS_COMMON_CATEGORIES_GROUPS_LABEL')</label>
+
+                {{ Select::subscriptionGroups('groups[]', $item->groups, array('multiple' => true)) }}
+            </div>
+
+            <div class="akeeba-form-group">
+                <label for="redirect_unauth">@lang('COM_ARS_COMMON_REDIRECT_UNAUTH')</label>
+
+                <input type="text" name="redirect_unauth" id="redirect_unauth"
+                       value="{{{ $item->redirect_unauth }}}" />
+            </div>
+
+            <div class="akeeba-form-group">
+                <label for="is_supported">@lang('COM_ARS_IS_SUPPORTED')</label>
+
+                @jhtml('FEFHelper.select.booleanswitch', 'is_supported', $item->is_supported)
+            </div>
+
+            <div class="akeeba-form-group">
+                <label for="language">@lang('JFIELD_LANGUAGE_LABEL')</label>
+
+                {{ Select::languages('language', $item->language) }}
+            </div>
+        </div>
+
+        <div>
+            @jhtml('FEFHelper.edit.editor', 'description', $this->getItem()->description)
+        </div>
+    </div>
+
+    <div>
+        {{ Html::rules($item->asset_id, 'rules', 'Categories', 'com_ars', 'category') }}
+    </div>
+@stop
