31e3a878fe04a84858a85f5a4ed81aac96654dfb
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2019-08-16 16:41:08 +0300

    Convert view templates to Blade
    Download ID labels

---
+++ component/backend/ViewTemplates/DownloadIDLabels/default.blade.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * @package   AkeebaReleaseSystem
+ * @copyright Copyright (c)2010-2019 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+/** @var $this \Akeeba\ReleaseSystem\Admin\View\DownloadIDLabels\Html */
+
+defined('_JEXEC') or die;
+
+?>
+@extends('admin:com_ars/Common/browse')
+
+@section('browse-filters')
+    <div class="akeeba-filter-element akeeba-form-group">
+        @searchfilter('label', 'label', 'COM_ARS_DLIDLABELS_FIELD_LABEL')
+    </div>
+
+    <div class="akeeba-filter-element akeeba-form-group">
+        @searchfilter('username', 'username', 'JGLOBAL_USERNAME')
+    </div>
+
+    <div class="akeeba-filter-element akeeba-form-group">
+        @searchfilter('dlid', 'dlid', 'COM_ARS_DLIDLABELS_FIELD_DOWNLOAD_ID')
+    </div>
+@stop
+
+@section('browse-table-header')
+    <tr>
+        <th width="32">
+            @jhtml('FEFHelper.browse.checkall')
+        </th>
+        <th>
+            @sortgrid('label', 'COM_ARS_DLIDLABELS_FIELD_LABEL')
+        </th>
+        <th>
+            @sortgrid('user_id', 'JGLOBAL_USERNAME')
+        </th>
+        <th>
+            @sortgrid('dlid', 'COM_ARS_DLIDLABELS_FIELD_DOWNLOAD_ID')
+        </th>
+        <th width="8%">
+            @sortgrid('enabled', 'JPUBLISHED')
+        </th>
+        <th>
+            @lang('COM_ARS_DLIDLABELS_FIELD_RESET')
+        </th>
+    </tr>
+@stop
+
+@section('browse-table-body-withrecords')
+	<?php $i = 0; ?>
+    @foreach($this->items as $row)
+		<?php
+		/** @var \Akeeba\ReleaseSystem\Admin\Model\DownloadIDLabels $row */
+		$enabled = $this->container->platform->getUser()->authorise('core.edit.state', 'com_ars')
+		?>
+        <tr>
+            <td>
+                @jhtml('FEFHelper.browse.id', ++$i, $row->getId())
+            </td>
+            <td>
+                @if (($row->label == '_MAIN_') || $row->primary)
+                    <span class="akeeba-label--green">@lang('COM_ARS_DLIDLABELS_LBL_DEFAULT')</span>
+                @else
+                    <a href="index.php?option=com_ars&view=DownloadIDLabel&task=edit&id={{ $row->ars_dlidlabel_id }}">
+                        {{{ $row->label }}}
+                    </a>
+                @endif
+            </td>
+            <td>
+                {{ \Akeeba\ReleaseSystem\Admin\Helper\Html::renderUserRepeatable((int) ($row->user_id)) }}
+            </td>
+            <td>
+				<span class="{{ $row->primary ? 'akeeba-label--dark' : '' }}">
+					{{ $row->primary ? '' : $row->user_id . ':' }}{{ $row->dlid }}
+				</span>
+            </td>
+            <td>
+                @jhtml('FEFHelper.browse.published', $row->enabled, $i, '', $enabled)
+            </td>
+            <td>
+                <a href="index.php?option=com_ars&view=DownloadIDLabels&task=reset&id={{{ $row->ars_dlidlabel_id }}}&{{{ $this->getContainer()->platform->getToken(true) }}}=1"
+                   class="akeeba-btn--orange--small">
+                    <span class="akion-refresh"></span>
+                    @lang('COM_ARS_DLIDLABELS_FIELD_RESET')
+                </a>
+            </td>
+        </tr>
+    @endforeach
+@stop
---
+++ component/backend/ViewTemplates/DownloadIDLabels/form.blade.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @package   AkeebaReleaseSystem
+ * @copyright Copyright (c)2010-2019 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+use Joomla\CMS\Form\Field\UserField;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
+/** @var \Akeeba\ReleaseSystem\Admin\View\DownloadIDLabels\Html $this */
+
+defined('_JEXEC') or die;
+
+/** @var \Akeeba\ReleaseSystem\Admin\Model\DownloadIDLabels $item */
+$item = $this->getItem();
+
+$userField        = new UserField();
+$userField->id    = 'user_id';
+$userField->name  = 'user_id';
+$userField->value = $item->user_id;
+?>
+
+@extends('admin:com_ars/Common/edit')
+
+@section('edit-form-body')
+    <div class="akeeba-container--50-50">
+        <div>
+            <div class="akeeba-form-group">
+                <label for="label">
+                    @lang('COM_ARS_DLIDLABELS_FIELD_LABEL')
+                </label>
+
+                <input type="text" name="label" id="label" value="{{{ $item->label }}}" />
+            </div>
+
+            <div class="akeeba-form-group">
+                <label for="label">
+                    @lang('COM_ARS_DLIDLABELS_FIELD_USER_ID')
+                </label>
+
+                {{ $userField->input }}
+            </div>
+
+            <div class="akeeba-form-group">
+                <label for="title">
+                    @lang('JPUBLISHED')
+                </label>
+
+                @jhtml('FEFHelper.select.booleanswitch', 'enabled', $item->enabled)
+            </div>
+        </div>
+    </div>
+@stop
