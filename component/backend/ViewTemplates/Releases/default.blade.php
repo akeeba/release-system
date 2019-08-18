1c4821a7ac6c4c2f94cf127b245c34669b6e28f9
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2019-08-18 13:16:18 +0300

    Convert view templates to Blade
    Releases

---
+++ component/backend/ViewTemplates/Releases/default.blade.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * @package   AkeebaReleaseSystem
+ * @copyright Copyright (c)2010-2019 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+/** @var $this \Akeeba\ReleaseSystem\Admin\View\Releases\Html */
+
+defined('_JEXEC') or die;
+
+$user = $this->getContainer()->platform->getUser();
+$i = 0;
+
+?>
+@jhtml('formbehavior.chosen')
+
+@extends('admin:com_ars/Common/browse')
+
+@section('browse-filters')
+    <div class="akeeba-filter-element akeeba-form-group">
+        {{ \Akeeba\ReleaseSystem\Admin\Helper\Select::categories($this->filters['category_id'], 'category_id', ['onchange' => 'document.adminForm.submit()', 'class' => 'advancedSelect']) }}
+    </div>
+
+    <div class="akeeba-filter-element akeeba-form-group">
+        @searchfilter('version', 'version', 'COM_ARS_RELEASES_FIELD_VERSION')
+    </div>
+
+    <div class="akeeba-filter-element akeeba-form-group">
+        {{ \Akeeba\ReleaseSystem\Admin\Helper\Select::maturity('maturity', $this->filters['maturity'], ['onchange' => 'document.adminForm.submit()']) }}
+    </div>
+
+    <div class="akeeba-filter-element akeeba-form-group">
+        {{ \Akeeba\ReleaseSystem\Admin\Helper\Select::accessLevel('access', $this->filters['access'], ['onchange' => 'document.adminForm.submit()', 'class' => 'advancedSelect']) }}
+    </div>
+@stop
+
+@section('browse-table-header')
+    <tr>
+        <th width="8%">
+            @sortgrid('ordering', '<i class="icon-menu-2"></i>')
+        </th>
+        <th width="32">
+            @jhtml('FEFHelper.browse.checkall')
+        </th>
+        <th>
+            @sortgrid('category_id', 'COM_ARS_RELEASES_FIELD_CATEGORY')
+        </th>
+        <th>
+            @sortgrid('version', 'COM_ARS_RELEASES_FIELD_VERSION')
+        </th>
+        <th>
+            @sortgrid('maturity', 'COM_ARS_RELEASES_FIELD_MATURITY')
+        </th>
+        <th>
+            @sortgrid('access', 'JFIELD_ACCESS_LABEL')
+        </th>
+        <th width="8%">
+            @sortgrid('published', 'JPUBLISHED')
+        </th>
+        <th>
+            @sortgrid('hits', 'JGLOBAL_HITS')
+        </th>
+        <th>
+            @sortgrid('language', 'JFIELD_LANGUAGE_LABEL')
+        </th>
+    </tr>
+@stop
+
+@section('browse-table-body-withrecords')
+    @foreach($this->items as $row)
+		<?php
+		/** @var \Akeeba\ReleaseSystem\Admin\Model\Releases $row */
+		?>
+        <tr>
+            <td>
+                @jhtml('FEFHelper.browse.order', 'ordering', $row->ordering)
+            </td>
+            <td>
+                @jhtml('FEFHelper.browse.id', ++$i, $row->getId())
+            </td>
+            <td>
+                {{{ \Akeeba\ReleaseSystem\Admin\Model\Categories::forceEagerLoad($row->category_id, 'title') }}}
+            </td>
+            <td>
+                @if ($user->authorise('core.admin') || $user->authorise('core.edit', 'com_ars.category.' . $row->category_id))
+                    <a href="index.php?option=com_ars&view=Release&id={{{ $row->getId() }}}">
+                        {{{ $row->version }}}
+                    </a>
+                @else
+                    {{{ $row->version }}}
+                @endif
+            </td>
+            <td>
+                @if (in_array($row->maturity, ['alpha', 'beta', 'rc', 'stable']))
+                    @lang('COM_ARS_RELEASES_MATURITY_'. $row->maturity)
+                @else
+                    @lang('COM_ARS_RELEASES_MATURITY_ALPHA')
+                @endif
+            </td>
+            <td>
+                {{ \Akeeba\ReleaseSystem\Admin\Helper\Html::accessLevel($row->access) }}
+            </td>
+            <td>
+                @jhtml('jgrid.published', $row->published, $i, '', $user->authorise('core.edit.state', 'com_ars'), 'cb')
+            </td>
+            <td>
+                {{{ $row->hits }}}
+            </td>
+            <td>
+                {{ \Akeeba\ReleaseSystem\Admin\Helper\Html::language($row->language) }}
+            </td>
+        </tr>
+    @endforeach
+@stop
---
+++ component/backend/ViewTemplates/Releases/form.blade.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * @package   AkeebaReleaseSystem
+ * @copyright Copyright (c)2010-2019 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+use Akeeba\ReleaseSystem\Admin\Helper\Select;
+use Joomla\CMS\Editor\Editor;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
+/** @var \Akeeba\ReleaseSystem\Admin\View\VisualGroups\Html $this */
+
+defined('_JEXEC') or die;
+
+/** @var \Akeeba\ReleaseSystem\Admin\Model\Releases $item */
+$item = $this->getItem();
+?>
+
+@jhtml('formbehavior.chosen')
+
+@extends('admin:com_ars/Common/edit')
+
+@section('edit-form-body')
+    <div class="akeeba-container--50-50">
+        <div>
+            <div class="akeeba-form-group">
+                <label for="category_id">@lang('COM_ARS_RELEASES_FIELD_CATEGORY')</label>
+
+                {{ \Akeeba\ReleaseSystem\Admin\Helper\Select::categories($item->category_id, 'category_id', ['class' => 'advancedSelect']) }}
+            </div>
+
+            <div class="akeeba-form-group">
+                <label for="version">@lang('COM_ARS_RELEASES_FIELD_VERSION')</label>
+
+                <input type="text" name="version" id="version" value="{{{ $item->version }}}" />
+            </div>
+
+            <div class="akeeba-form-group">
+                <label for="alias">@lang('COM_ARS_RELEASES_FIELD_ALIAS')</label>
+
+                <input type="text" name="alias" id="alias" value="{{{ $item->alias }}}" />
+            </div>
+
+            <div class="akeeba-form-group">
+                <label for="maturity">@lang('COM_ARS_RELEASES_FIELD_MATURITY')</label>
+
+                {{ \Akeeba\ReleaseSystem\Admin\Helper\Select::maturity('maturity', $item->maturity) }}
+            </div>
+
+            <div class="akeeba-form-group">
+                <label for="hits">@lang('JGLOBAL_HITS')</label>
+
+                <input type="text" name="hits" id="hits" value="{{{ $item->hits }}}" />
+            </div>
+
+            <div class="akeeba-form-group">
+                <label for="published">@lang('JPUBLISHED')</label>
+
+                @jhtml('FEFHelper.select.booleanswitch', 'published', $item->published)
+            </div>
+        </div>
+
+        <div>
+            <div class="akeeba-form-group">
+                <label for="access">@lang('JFIELD_ACCESS_LABEL')</label>
+
+                {{ \Akeeba\ReleaseSystem\Admin\Helper\Select::accessLevel('access', $item->access) }}
+            </div>
+
+            <div class="akeeba-form-group">
+                <label for="show_unauth_links">@lang('COM_ARS_COMMON_SHOW_UNAUTH_LINKS')</label>
+
+                @jhtml('FEFHelper.select.booleanswitch', 'show_unauth_links', $item->show_unauth_links)
+            </div>
+
+            <div class="akeeba-form-group">
+                <label for="redirect_unauth">@lang('COM_ARS_COMMON_REDIRECT_UNAUTH')</label>
+
+                <input type="text" name="redirect_unauth" id="redirect_unauth"
+                       value="{{{ $item->redirect_unauth }}}" />
+            </div>
+
+            <div class="akeeba-form-group">
+                <label for="groups">@lang('COM_ARS_COMMON_CATEGORIES_GROUPS_LABEL')</label>
+
+                {{ \Akeeba\ReleaseSystem\Admin\Helper\Select::subscriptionGroups('groups[]', $item->groups, array('multiple' => true, 'class' => 'advancedSelect')) }}
+            </div>
+
+            <div class="akeeba-form-group">
+                <label for="created">@lang('COM_ARS_RELEASES_FIELD_RELEASED')</label>
+
+                @jhtml('calendar', $item->created, 'created', 'created')
+            </div>
+
+            <div class="akeeba-form-group">
+                <label for="language">@lang('JFIELD_LANGUAGE_LABEL')</label>
+
+                {{ \Akeeba\ReleaseSystem\Admin\Helper\Select::languages('language', $item->language) }}
+            </div>
+        </div>
+    </div>
+
+    <div class="akeeba-container--50-50">
+        <div>
+            @jhtml('FEFHelper.edit.editor', 'description', $this->getItem()->description)
+        </div>
+        <div>
+            @jhtml('FEFHelper.edit.editor', 'notes', $this->getItem()->description)
+        </div>
+    </div>
+@stop
