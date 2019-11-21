7413a5315b792277c0f44ad585264dc355c9e729
Davide Tampellini <tampe125@gmail.com>
2019-11-21 10:22:27 +0100

    Removed EAcceleration check
    gh-168

--- component/backend/ars.php
+++ component/backend/ars.php
@@ -15,24 +15,6 @@ if (version_compare(PHP_VERSION, '7.3.0', 'lt'))
 	return;
 }
 
-// Why, oh why, are you people using eAccelerator? Seriously, what's wrong with you, people?!
-if (function_exists('eaccelerator_info'))
-{
-	$isBrokenCachingEnabled = true;
-
-	if (function_exists('ini_get') && !ini_get('eaccelerator.enable'))
-	{
-		$isBrokenCachingEnabled = false;
-	}
-
-	if ($isBrokenCachingEnabled)
-	{
-		(include_once __DIR__ . '/View/eaccelerator.php') or die('eAccelerator is broken and abandoned since 2012. Ask your host to disable it before using this component.');
-
-		return;
-	}
-}
-
 // HHVM made sense in 2013, now PHP 7 is a way better solution than an hybrid PHP interpreter
 if (defined('HHVM_VERSION'))
 {
