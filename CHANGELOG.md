becd0cd618f0c61fdf35e85f8e3569bda0ef192b
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2020-12-07 17:11:20 +0200

    ~ Better heading level relations in the Items page
    Close gh-182

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -15,6 +15,7 @@
 * `~` Optionally use com_compatibility to reduce the number of displayed releases in the update stream
 * `~` Minified JS files
 * `~` Replace zero datetime with nullable datetime (gh-179)
+* `~` Better heading level relations in the Items page (gh-182)
 * `#` [LOW] Frontend add-on download ID, redirect to non-SEF URL after editing an item
 * `#` [LOW] Frontend, ARS item selection is broken
 * `#` [LOW] No default list limit showing releases 
--- component/frontend/ViewTemplates/Items/default.blade.php
+++ component/frontend/ViewTemplates/Items/default.blade.php
@@ -38,20 +38,20 @@ $this->getContainer()->template->addJSInline($js);
 
 <div class="item-page{{{ $this->params->get('pageclass_sfx') }}}">
 	@if($this->params->get('show_page_heading'))
-	<div class="page-header">
-		<h1>
-			{{{ $this->params->get('page_heading', $this->menu->title) . ' ' . $this->release->version }}}
-		</h1>
-	</div>
+		<div class="page-header">
+			<h2>
+				{{{ $this->params->get('page_heading', $this->menu->title) . ' ' . $this->release->version }}}
+			</h2>
+		</div>
 	@endif
 
 	@include('site:com_ars/Items/release', ['id' => $this->release->id, 'item' => $this->release, 'Itemid' => $this->Itemid, 'no_link' => true])
 
 	<div class="ars-items ars-items-{{ $this->release->category->is_supported ? 'supported' : 'unsupported' }}">
-	@if(count($this->items))
-		@foreach($this->items as $item)
-			@include('site:com_ars/Items/item', ['item' => $item, 'Itemid' => $this->Itemid])
-		@endforeach
+		@if(count($this->items))
+			@foreach($this->items as $item)
+				@include('site:com_ars/Items/item', ['item' => $item, 'Itemid' => $this->Itemid])
+			@endforeach
 	@else
 		<div class="ars-noitems">
 			@lang('ARS_NO_ITEMS')
