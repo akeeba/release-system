932ac2a24da06dd868051cdb76d1999b78ec2dfd
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2021-02-24 12:05:40 +0200

    Fix copyright header

--- component/backend/fields/fancyradio.php
+++ component/backend/fields/fancyradio.php
@@ -5,12 +5,6 @@
  * @license   GNU General Public License version 3, or later
  */
 
-/**
- * @package   AkeebaSubs
- * @copyright Copyright (c)2010-2021 Nicholas K. Dionysopoulos / Akeeba Ltd
- * @license   GNU General Public License version 3, or later
- */
-
 defined('_JEXEC') || die();
 
 require_once JPATH_LIBRARIES . '/fof40/Html/Fields/fancyradio.php';
