7c8a27ea6f23aec6996b874c1a10a9bb244c105f
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2021-05-19 17:32:52 +0300

    Converting to core MVC
    ARS download ID content plugin

---
+++ plugins/content/arsdlid/services/provider.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @package   AkeebaReleaseSystem
+ * @copyright Copyright (c)2010-2021 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Extension\PluginInterface;
+use Joomla\CMS\Extension\Service\Provider\MVCFactory;
+use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+use Joomla\Event\DispatcherInterface;
+use Joomla\Plugin\Content\Arsdlid\Extension\Arsdlid;
+
+return new class implements ServiceProviderInterface {
+	/**
+	 * Registers the service provider with a DI container.
+	 *
+	 * @param   Container  $container  The DI container.
+	 *
+	 * @return  void
+	 *
+	 * @since   9.0.0
+	 */
+	public function register(Container $container)
+	{
+		$container->registerServiceProvider(new MVCFactory('Akeeba\\Component\\ARS'));
+
+		$container->set(
+			PluginInterface::class,
+			function (Container $container) {
+				$plugin     = PluginHelper::getPlugin('content', 'arsdlid');
+				$dispatcher = $container->get(DispatcherInterface::class);
+				$factory    = $container->get(MVCFactoryInterface::class);
+
+				return new Arsdlid(
+					$dispatcher,
+					(array) $plugin,
+					$factory
+				);
+			}
+		);
+	}
+};
---
+++ plugins/content/arsdlid/src/Extension/Arsdlid.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * @package   AkeebaReleaseSystem
+ * @copyright Copyright (c)2010-2021 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+namespace Joomla\Plugin\Content\Arsdlid\Extension;
+
+defined('_JEXEC') || die;
+
+use Akeeba\Component\ARS\Site\Model\DlidlabelsModel;
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Factory;
+use Joomla\CMS\MVC\Factory\MVCFactoryAwareTrait;
+use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\Event\Event;
+use Joomla\Event\SubscriberInterface;
+use Joomla\String\StringHelper;
+
+class Arsdlid extends CMSPlugin implements SubscriberInterface
+{
+	use MVCFactoryAwareTrait;
+
+	/**
+	 * Cache of user IDs to Download IDs
+	 *
+	 * @var   array
+	 */
+	private static $cache = [];
+
+	/** @var DlidlabelsModel */
+	private static $model;
+
+	public function __construct(&$subject, $config, MVCFactoryInterface $mvcFactory)
+	{
+		parent::__construct($subject, $config);
+
+		$this->setMVCFactory($mvcFactory);
+
+		self::$model = $this->mvcFactory->createModel('Dlidlabels', 'site');
+	}
+
+	/**
+	 * Returns an array of events this subscriber will listen to.
+	 *
+	 * @return  array
+	 *
+	 * @since   9.0.0
+	 */
+	public static function getSubscribedEvents(): array
+	{
+		// Only subscribe events if the component is installed and enabled
+		if (!ComponentHelper::isEnabled('com_ars'))
+		{
+			return [];
+		}
+
+		return [
+			'onContentPrepare' => 'onContentPrepare',
+		];
+	}
+
+	private static function process(array $match): string
+	{
+		$user = Factory::getApplication()->getIdentity();
+
+		if ($user->guest)
+		{
+			return '';
+		}
+
+		self::$cache[$user->id] = self::$cache[$user->id] ?? self::$model->myDownloadID($user);
+
+		return self::$cache[$user->id];
+	}
+
+	public function onContentPrepare(Event $event)
+	{
+		[$context, $article, $params, $limitstart] = $event->getArguments();
+
+		if (!ComponentHelper::isEnabled('com_ars'))
+		{
+			return;
+		}
+
+		// Check whether the plugin should process or not
+		if (StringHelper::strpos($article->text, 'downloadid') === false)
+		{
+			return;
+		}
+
+		// Search for this tag in the content
+		$regex = "#{[\s]*downloadid[\s]*}#s";
+
+		$article->text = preg_replace_callback($regex, ['self', 'process'], $article->text);
+	}
+}
