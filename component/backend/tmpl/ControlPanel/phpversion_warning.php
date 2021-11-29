446542e0293571afa5b2bd9115b10611f2cb8fd8
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2021-11-29 21:17:47 +0200

    Common error pages

--- component/backend/tmpl/ControlPanel/phpversion_warning.php
+++ component/backend/tmpl/ControlPanel/phpversion_warning.php
@@ -61,6 +61,7 @@ if (!function_exists('akeeba_common_phpversion_warning'))
 			'7.3' => array('2020-12-06 00:00:00', '2021-12-06 00:00:00'),
 			'7.4' => array('2021-11-28 00:00:00', '2022-11-28 00:00:00'),
 			'8.0' => array('2022-11-26 00:00:00', '2023-11-26 00:00:00'),
+			'8.1' => array('2023-11-25 00:00:00', '2024-11-25 00:00:00'),
 		);
 
 		// Make sure I have all necessary configuration variables
