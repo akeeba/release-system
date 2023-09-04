515e8a7182910e83290fcc733d5292f0ac537aa6
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2023-09-04 11:05:08 +0300

    Make sure Joomla will invalidate the autoload_psr4.php file on update.

--- component/script.ars.php
+++ component/script.ars.php
@@ -63,13 +63,34 @@ class Pkg_ArsInstallerScript extends \Joomla\CMS\Installer\InstallerScript
 			try
 			{
 				$nsMap = new JNamespacePsr4Map();
+
+				@clearstatcache(JPATH_CACHE . '/autoload_psr4.php');
+
+				if (function_exists('opcache_invalidate'))
+				{
+					@opcache_invalidate(JPATH_CACHE . '/autoload_psr4.php');
+				}
+
+				@clearstatcache(JPATH_CACHE . '/autoload_psr4.php');
 				$nsMap->create();
+
+				if (function_exists('opcache_invalidate'))
+				{
+					@opcache_invalidate(JPATH_CACHE . '/autoload_psr4.php');
+				}
+
 				$nsMap->load();
 			}
 			catch (\Throwable $e)
 			{
 				// In case of failure, just try to delete the old autoload_psr4.php file
+				if (function_exists('opcache_invalidate'))
+				{
+					@opcache_invalidate(JPATH_CACHE . '/autoload_psr4.php');
+				}
+
 				@unlink(JPATH_CACHE . '/autoload_psr4.php');
+				@clearstatcache(JPATH_CACHE . '/autoload_psr4.php');
 			}
 		}
 
