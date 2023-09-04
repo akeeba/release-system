118c94026be5a1ccb6f3f7374dbbfbe9f747bd90
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2023-09-04 14:08:36 +0300

    Workaround for Joomla extensions update sucking
    Joomla does not OPcache-invalidate files when updating an extension. I have to do it by myself.

--- component/script.ars.php
+++ component/script.ars.php
@@ -94,6 +94,8 @@ class Pkg_ArsInstallerScript extends \Joomla\CMS\Installer\InstallerScript
 			}
 		}
 
+		$this->invalidateFiles();
+
 		// Remove obsolete update site
 		$this->removeOldUpdateSites();
 
@@ -309,4 +311,155 @@ class Pkg_ArsInstallerScript extends \Joomla\CMS\Installer\InstallerScript
 		}
 	}
 
+	private function invalidateFiles()
+	{
+		function getManifestXML($class): ?SimpleXMLElement
+		{
+			// Get the package element name
+			$myPackage = strtolower(str_replace('InstallerScript', '', $class));
+
+			// Get the package's manifest file
+			$filePath = JPATH_MANIFESTS . '/packages/' . $myPackage . '.xml';
+
+			if (!@file_exists($filePath) || !@is_readable($filePath))
+			{
+				return null;
+			}
+
+			$xmlContent = @file_get_contents($filePath);
+
+			if (empty($xmlContent))
+			{
+				return null;
+			}
+
+			return new SimpleXMLElement($xmlContent);
+		}
+
+		function xmlNodeToExtensionName(SimpleXMLElement $fileField): ?string
+		{
+			$type = (string) $fileField->attributes()->type;
+			$id   = (string) $fileField->attributes()->id;
+
+			switch ($type)
+			{
+				case 'component':
+				case 'file':
+				case 'library':
+					$extension = $id;
+					break;
+
+				case 'plugin':
+					$group     = (string) $fileField->attributes()->group ?? 'system';
+					$extension = 'plg_' . $group . '_' . $id;
+					break;
+
+				case 'module':
+					$client    = (string) $fileField->attributes()->client ?? 'site';
+					$extension = (($client != 'site') ? 'a' : '') . $id;
+					break;
+
+				default:
+					$extension = null;
+					break;
+			}
+
+			return $extension;
+		}
+
+		function getExtensionsFromManifest(?SimpleXMLElement $xml): array{
+			if (empty($xml))
+			{
+				return [];
+			}
+
+			$extensions = [];
+
+			foreach ($xml->xpath('//files/file') as $fileField)
+			{
+				$extensions[] = xmlNodeToExtensionName($fileField);
+			}
+
+			return array_filter($extensions);
+		}
+
+		function clearFileInOPCache(string $file): bool
+		{
+			static $hasOpCache = null;
+
+			if (is_null($hasOpCache)) {
+				$hasOpCache = ini_get('opcache.enable')
+				              && function_exists('opcache_invalidate')
+				              && (!ini_get('opcache.restrict_api') || stripos(realpath($_SERVER['SCRIPT_FILENAME']), ini_get('opcache.restrict_api')) === 0);
+			}
+
+			if ($hasOpCache && (strtolower(substr($file, -4)) === '.php')) {
+				$ret = opcache_invalidate($file, true);
+
+				@clearstatcache($file);
+
+				return $ret;
+			}
+
+			return false;
+		}
+
+		function recursiveClearCache(string $path): void
+		{
+			if (!@is_dir($path))
+			{
+				return;
+			}
+
+			/** @var DirectoryIterator $file */
+			foreach (new DirectoryIterator($path) as $file)
+			{
+				if ($file->isDot() || $file->isLink()) {
+					continue;
+				}
+
+				if ($file->isDir())
+				{
+					recursiveClearCache($file->getPathname());
+
+					continue;
+				}
+
+				if (!$file->isFile())
+				{
+					continue;
+				}
+
+				clearFileInOPCache($file->getPathname());
+			}
+		}
+
+		$extensionsFromPackage = getExtensionsFromManifest(getManifestXML(__CLASS__));
+
+		foreach ($extensionsFromPackage as $element)
+		{
+			if (strpos($element, 'plg_') !== 0)
+			{
+				continue;
+			}
+
+			[$dummy, $folder, $plugin] = explode('_', $element);
+
+			recursiveClearCache(
+				sprintf(
+					'%s/%s/%s/services',
+					JPATH_PLUGINS, $folder, $plugin
+				)
+			);
+
+			recursiveClearCache(
+				sprintf(
+					'%s/%s/%s/src',
+					JPATH_PLUGINS, $folder, $plugin
+				)
+			);
+		}
+
+		clearFileInOPCache(JPATH_CACHE . '/autoload_psr4.php');
+	}
 }
