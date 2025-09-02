e03e7ad02172b15323b24f867c9e24f37438b1b7
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2025-09-02 10:22:56 +0300

    ~ Preliminary support for Joomla 6
    HTMLHelper's formbehavior.multiselect has been removed

--- component/backend/tmpl/releases/default.php
+++ component/backend/tmpl/releases/default.php
@@ -23,7 +23,23 @@ if (version_compare(JVERSION, '4.999.999', 'lt'))
 	$this->loadAnyTemplate('common/joomla_eol');
 }
 
-HTMLHelper::_('behavior.multiselect');
+/**
+ * HTMLHelper's `behavior.multiselect` is deprecated in Joomla 6.
+ *
+ * See Joomla PR 45925.
+ */
+call_user_func(function(string $formName = 'adminForm') {
+	if (version_compare(JVERSION, '5.999.999', 'lt'))
+	{
+		HTMLHelper::_('behavior.multiselect');
+
+		return;
+	}
+
+	$doc       = Factory::getApplication()->getDocument();
+	$doc->addScriptOptions('js-multiselect', ['formName' => $formName]);
+	$doc->getWebAssetManager()->useScript('multiselect');
+});
 
 $user      = Factory::getApplication()->getIdentity();
 $userId    = $user->id;
--- component/frontend/tmpl/dlidlabels/default.php
+++ component/frontend/tmpl/dlidlabels/default.php
@@ -16,7 +16,24 @@ use Joomla\CMS\Router\Route;
 
 /** @var \Akeeba\Component\ARS\Site\View\Dlidlabels\HtmlView $this */
 
-HTMLHelper::_('behavior.multiselect');
+/**
+ * HTMLHelper's `behavior.multiselect` is deprecated in Joomla 6.
+ *
+ * See Joomla PR 45925.
+ */
+call_user_func(function(string $formName = 'adminForm') {
+	if (version_compare(JVERSION, '5.999.999', 'lt'))
+	{
+		HTMLHelper::_('behavior.multiselect');
+
+		return;
+	}
+
+	$doc       = Factory::getApplication()->getDocument();
+	$doc->addScriptOptions('js-multiselect', ['formName' => $formName]);
+	$doc->getWebAssetManager()->useScript('multiselect');
+});
+
 HTMLHelper::_('bootstrap.tooltip', '.hasTooltip');
 
 $app       = Factory::getApplication();
